c3457ed698a8b1b69971f98a54cb81e4256c3079
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2017-11-04 20:16:38 +0100

    Update

--- Figures_Lisa.m
+++ Figures_Lisa.m
@@ -21,6 +21,8 @@ fs=20000; %Sampling frequency of acquisition.
 
 addpath('/home/raleman/Documents/MATLAB/analysis-tools-master')  
 addpath('/home/raleman/Documents/GitHub/CorticoHippocampal')  
+addpath('/home/raleman/Documents/internship')  
+
 %%
 num=1;
 data_lisa(num)
@@ -89,33 +91,75 @@ downsamp(4);
 load('V9.mat')
 load('V17.mat')
 
+V9n=outlier(V9);
+V17n=outlier(V17);
+
+probad=isoutlier(V9,'quartile');
+%%
+load('V9.mat')
+load('V17.mat')
+
+%%
+V9n=outlier(V9);
+V17n=outlier(V17);
 
 fn=1000; % New sampling frequency. 
 Wn1=[100/(fn/2) 300/(fn/2)]; % Cutoff=500 Hz
 [b1,a1] = butter(3,Wn1,'bandpass'); %Filter coefficients for LPF
 
-Mono17=filtfilt(b1,a1,V17);
-Mono9=filtfilt(b1,a1,V9);
+Mono17=filtfilt(b1,a1,V17n);
+Mono9=filtfilt(b1,a1,V9n);
 
 
 %% SWR detection 
 
 signal=Mono17*(1/0.195);
 
-
+thr=60;
 t=(0:length(signal)-1)*(1/fn); %IN SECONDS
-[S, E, M] = findRipplesLisa(signal, t, 150 , 150*(1/3), []);
+[S, E, M] = findRipplesLisa(signal, t, thr , thr*(1/3), []);
 
 %%
+% label1='PFC';
+% label2= strcat('Thr:',num2str(thr));
+num=500;
 
-[cellx,cellr]=winL(M,Mono9,V9);
+label1='Hippocampus';
+label2=strcat('Thr:',num2str(thr));
+Mono17=Mono17*(1/0.195);
+V17n=V17n*(1/0.195);
 
-[p3 ,p4]=eta(cellx,cellr);
+
+[cellx,cellr]=winL(M,Mono17,V17n,num);
+[cellx,cellr]=clean(cellx,cellr);
+
+allscreen
+[p3 ,p4]=eta(cellx,cellr,num);
+mtit(strcat(label1),'fontsize',14,'color',[1 0 0],'position',[.5 1 ])
+mtit(strcat(' (',label2,')'),'fontsize',14,'color',[1 0 0],'position',[.5 0.8 ])
+
+barplot2(p4,p3,num)
+
+[cfs,f]=barplot3(p4,p3,num);
+%%
+saveas(gcf,'Barchart.png')
+%%
+
+[cellx,cellr]=winL(M,Mono17,V17n,500);
+[cellx,cellr]=clean(cellx,cellr);
+
+%
+[p3 ,p4]=eta(cellx,cellr,500);
+
+%%
 barplot2(p4,p3)
 [cfs,f]=barplot3(p4,p3);
+
 %%
 
-[cellx,cellr]=winL(M,Mono17,V17);
+%%
+
+[cellx,cellr]=winL(M,Mono17,V17n);
 
 [p3 ,p4]=eta(cellx,cellr);
 barplot2(p4,p3)
