5439d0a0177c35ea3c31b2505aa7d7e878064a0d
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2017-11-08 18:55:38 +0100

    Update the HC

--- plot_all.m
+++ plot_all.m
@@ -1,31 +1,44 @@
 function plot_all(folder)
+fs= gethpath()
 
-str2=cell(13,1);
+thresholds=[ 138 120 138 110 124 115 105 105 105 108 165 110 228 190 176 172 142 206 260 175 140 110 130 140 187 150 ];
+
+out=[ones(12,1)*5 ;4.5; 4.5; 4; ones(5,1)*4.5; ones(6,1)*5 ];
+
+str2=cell(26,1);
 str2{1,1}='/home/raleman/Documents/internship/Lisa_files/data/PT1';
 str2{2,1}='/home/raleman/Documents/internship/Lisa_files/data/PT2';
 str2{3,1}='/home/raleman/Documents/internship/Lisa_files/data/PT3';
 str2{4,1}='/home/raleman/Documents/internship/Lisa_files/data/PT4';
-str2{5,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5';
+%str2{5,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5';
 
-str2{6,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_1';
-str2{7,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_2';
-str2{8,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_3';
-str2{9,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_4';
-str2{10,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_5';
-str2{11,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_6';
+str2{5,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_1';
+str2{6,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_2';
+str2{7,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_3';
+str2{8,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_4';
+str2{9,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_5';
+str2{10,1}='/home/raleman/Documents/internship/Lisa_files/data/PT5_6';
 
-str2{12,1}='/home/raleman/Documents/internship/Lisa_files/data/Presleep1';
-str2{13,1}='/home/raleman/Documents/internship/Lisa_files/data/Presleep2';
+str2{11,1}='/home/raleman/Documents/internship/Lisa_files/data/Presleep1';
+str2{12,1}='/home/raleman/Documents/internship/Lisa_files/data/Presleep2';
 
 %Control
-str2{14,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_retest';
-str2{15,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial1';
-str2{16,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial2';
-str2{17,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial3';
-str2{18,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial4';
-str2{19,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial5';
-str2{20,1}='/home/raleman/Documents/internship/Lisa_files/day 4/presleep_22';
-str2{21,1}='/home/raleman/Documents/internship/Lisa_files/day 4/presleep_28';
+str2{13,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_retest';
+str2{14,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial1';
+str2{15,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial2';
+str2{16,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial3';
+str2{17,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial4';
+str2{18,1}='/home/raleman/Documents/internship/Lisa_files/day 4/post_trial5';
+str2{19,1}='/home/raleman/Documents/internship/Lisa_files/day 4/presleep_22';
+str2{20,1}='/home/raleman/Documents/internship/Lisa_files/day 4/presleep_28';
+
+%Hc
+str2{21,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/post_trial1';
+str2{22,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/post_trial2';
+str2{23,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/post_trial3';
+str2{24,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/post_trial4';
+str2{25,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/post_trial5';
+str2{26,1}='/home/raleman/Documents/internship/Lisa_files/day 3 HC/presleep';
 
 %str2{6,1}='/home/raleman/Documents/internship/Lisa_files/data/PT6';
 
@@ -47,8 +60,8 @@ V9=filtfilt(b1,a1,V9);
 
 % % 
 
-V9n=outlier(V9);
-V17n=outlier(V17);
+V9n=outlier(V9,out(folder));
+V17n=outlier(V17,out(folder));
 % 
 % V9n=V9;
 % V17n=V17;
@@ -65,9 +78,16 @@ Mono9=filtfilt(b1,a1,V9n);
 
 %Ripple detection 
 signal=Mono17*(1/0.195);
-thr=175;
+thr=thresholds(1,folder);
+%thr=150;
 t=(0:length(signal)-1)*(1/fn); %IN SECONDS
+if folder<=12   
+    [S, E, M] = findRipplesLisa(signal, t, thr , thr*(1/2), []);
+
+else
 [S, E, M] = findRipplesLisa(signal, t, thr , thr*(1/2), []);
+    
+end
 % 
 % remshort=E-S;
 % %remindex=(remshort>0.06);
@@ -106,7 +126,7 @@ mtit(strcat('Events:',num2str(ripples)),'fontsize',14,'color',[1 0 0],'position'
 barplot2(p4,p3,num,1000)
 
 [cfs,f]=barplot3(p4,p3,num);
-saveas(gcf,strcat(num2str(num),'Hippo','.png'));
+saveas(gcf,strcat(num2str(num),'COLORHippo','.png'));
 
 %Save PFC 
 label1='PFC';
@@ -126,7 +146,7 @@ mtit(strcat('Events:',num2str(ripples)),'fontsize',14,'color',[1 0 0],'position'
 barplot2(p4,p3,num,1000)
 
 [cfs,f]=barplot3(p4,p3,num);
-saveas(gcf,strcat(num2str(num),'PFC','.png'));
+saveas(gcf,strcat(num2str(num),'COLORPFC','.png'));
 % error('stop')
 
 
