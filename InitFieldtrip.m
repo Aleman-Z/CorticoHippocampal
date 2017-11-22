d6e9c64727cfa4920f9aeab120e162c75d430014
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2017-11-22 16:00:45 +0100

    Add Fieldtrip to path

---
+++ InitFieldtrip.m
@@ -0,0 +1,24 @@
+function InitFieldtrip(varargin)
+cd fieldtrip-master
+cfg             = [];
+cfg.ntrials     = 500;
+cfg.triallength = 1;
+cfg.fsample     = 200;
+cfg.nsignal     = 3;
+cfg.method      = 'ar';
+
+cfg.params(:,:,1) = [ 0.8    0    0 ; 
+                        0  0.9  0.5 ;
+                      0.4    0  0.5];
+                      
+cfg.params(:,:,2) = [-0.5    0    0 ; 
+                        0 -0.8    0 ; 
+                        0    0 -0.2];
+                        
+cfg.noisecov      = [ 0.3    0    0 ;
+                        0    1    0 ;
+                        0    0  0.2];
+
+data              = ft_connectivitysimulation(cfg);
+cd ..
+end
