ab786f03317563e55f01618ab15508d8aa59fca9
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2017-11-22 21:33:02 +0100

    autoregressive model

---
+++ createauto.m
@@ -0,0 +1,16 @@
+function [granger2]=createauto(data1,order)
+%Parametric model Order 20
+cfg2         = [];
+cfg2.order   = order;
+cfg2.toolbox = 'bsmart';
+mdata2       = ft_mvaranalysis(cfg2, data1);
+
+cfg2        = [];
+cfg2.method = 'mvar';
+mfreq2      = ft_freqanalysis(cfg2, mdata2);
+
+cfg2           = [];
+cfg2.method    = 'granger';
+granger2       = ft_connectivityanalysis(cfg2, mfreq2);
+
+end
