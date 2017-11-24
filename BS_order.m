5de36f4b738c3081add570bd18dfc591b7b190de
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2017-11-24 15:18:46 +0100

    Bsmart adaptation

---
+++ BS_order.m
@@ -0,0 +1,27 @@
+%AIC test
+function BS_order(dat,winlen,maxorder)
+
+winlen   = 12;
+maxorder = 10;
+points   = size(dat,1);
+aic = aic_test(dat,winlen,maxorder);
+
+%
+
+wind = 5; %Select window size. 
+
+if (wind > (points-winlen+1) || wind <= 0)
+    errordlg('please input correct window number','parameter lost');
+    return
+end
+
+dat = aic(wind,:);
+data=dat(~isnan(dat));
+
+% draw the figure
+figure('Name','AIC Model Order Estimation','NumberTitle','off')
+plot(data,'-s');
+h = gca;
+xlabel(h,'Model Order');
+ylabel(h,'AIC measure');
+end
---
+++ BS_pre.m
@@ -0,0 +1,7 @@
+data1 = pre_sube(dat);
+
+data2 = pre_sube_divs(dat);
+
+data3 = pre_subt(dat);
+
+data4 = pre_subt_divs(dat);
