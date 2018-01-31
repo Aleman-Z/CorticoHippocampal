8119059ccaad924b1d45b96501dacae23724fe63
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-02-01 00:30:28 +0100

    Rat 27

---
+++ read_channels_ft_27.m
@@ -0,0 +1,509 @@
+addpath('/home/raleman/Documents/MATLAB/analysis-tools-master'); %Open Ephys data loader. 
+addpath('/home/raleman/Documents/GitHub/CorticoHippocampal')
+addpath('/home/raleman/Documents/internship')
+
+%%
+nFF=[
+    {'rat27_nl_base_2016-03-28_15-01-17'                   }
+    {'rat27_NL_baseline_2016-02-26_12-50-26'               }
+    {'rat27_nl_base_III_2016-03-30_14-36-57'               }
+    
+    {'rat27_for_2016-03-21_15-03-05'                       }
+    {'Rat27_for_II_2016-03-23_15-06-59'                    }
+    
+    %{'rat27_novelty_II_2016-04-13_14-37-58'                }  %NO .MAT files found. 
+    %{'rat27_novelty_II_2016-04-13_16-29-42'                } %No (complete).MAT files found.
+    {'rat27_novelty_I_2016-04-11_14-34-55'                 }
+    {'rat27_plusmaze_base_2016-03-14_14-52-48'             }
+    {'rat27_plusmaze_base_II_2016-03-24_14-10-08'          }
+%     {'rat27_plusmaze_dis_2016-03-10_14-35-18'              }
+%     {'rat27_plusmaze_dis_II_2016-03-16_14-36-07'           }
+%     {'rat27_plusmaze_dis_II_2016-03-18_14-46-24'           }
+%     {'rat27_plusmaze_jit_2016-03-08_14-46-31'              }
+%     {'rat27_plusmaze_jit_II_2016-03-16_15-02-27'           }
+%     {'rat27_plusmaze_swrd_qPCR_2016-04-15_14-28-41'        }
+%     {'rat27_watermaze_dis_morning_2016-04-06_10-18-36'     }
+%     {'rat27_watermaze_jitter_afternoon_2016-04-06_15-41-51'}  
+    ]
+
+labelconditions=[
+    {'Foraging 1' }
+    
+     'Foraging 2'
+     'Novelty_1'
+     'PlusMaze 1'
+    'PlusMaze 2'
+    
+     
+    ];
+%%
+cd('/home/raleman/Documents/internship/27')
+addpath /home/raleman/Documents/internship/fieldtrip-master/
+InitFieldtrip()
+
+cd('/home/raleman/Documents/internship/27')
+%% Select experiment to perform. 
+inter=0;
+granger=0;
+%
+%for iii=1:length(nFF)
+for iii=2:2
+    
+ clearvars -except nFF iii labelconditions inter granger 
+%for iii=1:4
+
+cd('/home/raleman/Documents/internship/27')
+cd(nFF{iii})
+
+art=0;
+
+Ro=[1200];
+ro=Ro;
+
+%run('load_data.m')
+%run('new_load_data.m')
+% error('stop')
+if ro==1200 && inter==0 || granger==1
+run('newest_load_data.m')
+else
+run('newest_load_data_only_ripple.m')
+end
+% Rearrange (clean)
+
+%Make labels
+label1=cell(7,1);
+label1{1}='Hippo';
+label1{2}='Hippo';
+label1{3}='Parietal';
+label1{4}='Parietal';
+label1{5}='PFC';
+label1{6}='PFC';
+label1{7}='Reference';
+
+label2=cell(7,1);
+label2{1}='Monopolar';
+label2{2}='Bipolar';
+label2{3}='Monopolar';
+label2{4}='Bipolar';
+label2{5}='Monopolar';
+label2{6}='Bipolar';
+label2{7}='Monopolar';
+
+sig1=cell(7,1);
+
+sig1{1}=Mono17;
+sig1{2}=Bip17;
+sig1{3}=Mono12;
+sig1{4}=Bip12;
+sig1{5}=Mono9;
+sig1{6}=Bip9;
+sig1{7}=Mono6;
+
+
+sig2=cell(7,1);
+
+sig2{1}=V17;
+sig2{2}=S17;
+sig2{3}=V12;
+% sig2{4}=R12;
+sig2{4}=S12;
+%sig2{6}=SSS12;
+sig2{5}=V9;
+% sig2{7}=R9;
+sig2{6}=S9;
+%sig2{10}=SSS9;
+sig2{7}=V6;
+ 
+% ripple=length(M);
+
+%Number of ripples per threshold.
+ripple=sum(s17);
+% ripple2=sum(s217);
+
+
+ % Monopolar signals only
+%Ro=[200 500];
+
+%for i=1:length(Ro)
+%i=1;
+    
+ %for level=1:length(ripple);
+ for level=1:length(ripple)-1;
+   
+%   allscreen()
+  %[p,q,timecell,cfs,f]=getwin(carajo,veamos,sig1,sig2,label1,label2,ro);
+  %Get p and q.
+  %Get averaged time signal. 
+[p,q,timecell,Q,~,~]=getwin2(carajo{:,:,level},veamos{level},sig1,sig2,label1,label2,ro,ripple(level),thr(level+1));
+% SUS=SU(:,level).';
+
+if ro==1200 && inter==0 || granger==1
+SUS=SU{level}.';
+% SUS2=SU2(:,level).';
+SUS2=SU2{level}.';
+%SUQ2=SUQ(:,level).';
+SUQ2=SUQ{level}.';
+
+SUStimecell=timecell(1:length(SUS));
+SUS2timecell=timecell(1:length(SUS2));
+SUQ2timecell=timecell(1:length(SUQ2));
+
+P1_SUS=avg_samples(SUS,SUStimecell);
+P2_SUS=avg_samples(SUS2,SUS2timecell);
+
+end
+%p is wideband signal. 
+%q is bandpassed signal. 
+%timecell are the time labels. 
+%Q is the envelope of the Bandpassed signal.  
+
+% [p2,q2,timecell2,Q2,~,~]=getwin2(carajo2{:,:,level},veamos2{level},sig1,sig2,label1,label2,ro,ripple2(level),thr2(level+1));
+
+%close all
+%P1 bandpassed
+%P2 widepassdw
+% error('stop here please')
+
+%Finding .mat files 
+Files=dir(fullfile(cd,'*.mat'));
+
+if ro==1200 
+    if sum(strcmp({Files.name}, strcat('randnum2_',num2str(level),'.mat')))
+      load(strcat('randnum2_',num2str(level),'.mat'))
+    else
+
+      ran=randi(length(p),100,1);
+      save(strcat('randnum2_',num2str(level)),'ran')
+
+    end
+
+else
+     if sum(strcmp({Files.name}, strcat('randnum17_',num2str(level),'.mat')))
+      load(strcat('randnum17_',num2str(level),'.mat'))
+    else
+
+      ran=randi(length(p),100,1);
+      save(strcat('randnum17_',num2str(level)),'ran')
+
+    end
+    
+end
+
+
+% 
+% ran=randi(length(p),100,1);
+%load(strcat('randnum2_',num2str(level),'.mat'))
+p=p([ran]);
+q=q([ran]);
+Q=Q([ran]);
+timecell=timecell([ran]);
+ 
+% p2=p2([ran]);
+% q2=q2([ran]);
+% Q2=Q2([ran]);
+% timecell2=timecell2([ran]);
+
+% p=p(1:100);
+% q=q(1:100);
+% Q=Q(1:100);
+% timecell=timecell(1:100);
+
+P1=avg_samples(q,timecell);
+P2=avg_samples(p,timecell);
+
+% SUStimecell=timecell(1:length(SUS));
+% SUS2timecell=timecell(1:length(SUS2));
+% SUQ2timecell=timecell(1:length(SUQ2));
+% 
+% P1_SUS=avg_samples(SUS,SUStimecell);
+% P2_SUS=avg_samples(SUS2,SUS2timecell);
+
+% P12=avg_samples(q2,timecell2);
+% P22=avg_samples(p2,timecell2);
+
+% save(strcat('randnum2_',num2str(level)),'ran')
+
+%for w=1:size(P2,1)-1
+for w=1:size(P2,1)
+% %     error('stop here')
+% run('plot_no_ripples.m')
+% string=strcat('NEW_SPEC_',label1{2*w-1},'_','NORIP',num2str(level),'.png');
+% %saveas(gcf,string)
+% close all
+% 
+% baseline1=[-1, -0.8];
+% baseline2=[0.8, 1];
+% 
+% run('plot_ripples.m')
+% 
+% string=strcat('NEW_SPEC_',label1{2*w-1},'_',num2str(level),'.png');
+% %saveas(gcf,string)
+
+%     error('stop here')
+
+% error('stop here')
+%%
+
+if iii>=4 && inter==1
+run('plot_inter_conditions_27.m')
+string=strcat('Intra_conditions_',label1{2*w-1},'_',num2str(level),'.png');
+saveas(gcf,string)
+end
+
+if ro==1700
+run('plot_pre_post.m')
+string=strcat('NEW2_pre_post_',label1{2*w-1},'_',num2str(level),'.png');
+saveas(gcf,string)
+end
+
+if ro==1200 && inter==0 && granger==0
+run('plot_both.m')
+% string=strcat('NEW2_between_',label1{2*w-1},'_',num2str(level),'.png');
+ string=strcat('NoRipple_',label1{2*w-1},'_',num2str(level),'.png');
+ saveas(gcf,string)
+end
+
+close all
+
+if w==1 && granger==1
+% allscreen()    
+% [granger]=barplot_GC(cut(q),cut(timecell),[100:20:300])
+% title(strcat('Time-Frequency Granger Causality (Bandpassed: 100-300 Hz)',' THR:',num2str(thr(level))))
+% 
+% string=strcat('TFGC_','Bandpass_',num2str(level),'.png');
+% saveas(gcf,string)
+% close all
+
+
+allscreen()    
+[coh]=barplot_COH(q,timecell,[100:2:300])
+title('Time-Frequency Coherence (Bandpassed: 100-300 Hz)')
+string=strcat('COH_','Bandpass_Ripple_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+allscreen()    
+[coh]=barplot_COH(SUS,SUStimecell,[100:2:300])
+title('Time-Frequency Coherence (Bandpassed: 100-300 Hz)')
+string=strcat('NEW_TFCOH_','Bandpass_NoRipple_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+% allscreen()
+% [granger]=barplot_GC(cut(p),cut(timecell),[2:2:30])
+% title('Time-Frequency Granger Causality (Wideband)')
+% string=strcat('TFGC_','Wideband_',num2str(level),'.png');
+% saveas(gcf,string)
+% close all
+
+allscreen()
+[coh]=barplot_COH(p,timecell,[1:1:30])
+title('Time-Frequency Coherence (Wideband)')
+string=strcat('NEW_TFCOH_','Wideband_Ripple_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+
+allscreen()
+[coh]=barplot_COH(SUS2,SUS2timecell,[1:1:30])
+title('Time-Frequency Coherence (Wideband)')
+string=strcat('NEW_TFCOH_','Wideband_NoRipple_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+
+%allscreen()
+gc(Q,timecell,'Envelope',ro)
+string=strcat('NEW_GC_','Envelope_Ripples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+%Here onwards (2)
+
+%allscreen()
+gc(SUQ2,SUQ2timecell,'Envelope',ro)
+string=strcat('NEW_GC_','Envelope_NoRipples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+
+%allscreen()
+gc(p,timecell,'Widepass',ro)
+string=strcat('NEW_GC_','Widepass_Ripples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+%allscreen()
+gc(SUS2,SUS2timecell,'Widepass',ro)
+string=strcat('NEW_GC_','Widepass_No_Ripples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+
+%allscreen()
+gc(q,timecell,'Bandpassed',ro)
+string=strcat('NEW_GC_','Bandpassed_Ripples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+%allscreen()
+gc(SUS,SUStimecell,'Bandpassed',ro)
+string=strcat('NEW_GC_','Bandpassed_NoRipples_',num2str(level),'.png');
+saveas(gcf,string)
+close all
+
+w=size(P2,1);
+end
+
+
+end
+%
+% chanindx = find(strcmp(freq.label, 'Hippo'));
+% figure; imagesc(squeeze(freq.powspctrm(1,chanindx,:,:)));
+%
+
+%figure; 
+
+%
+% subplot(3,2,4)
+% barplot2_ft(q,timecell,[100:1:300],w)
+
+
+%
+% barplot2_ft(q,timecell,[0:.1:30],2)
+% figure()
+% barplot2_ft(Q,timecell,[0:.01:30],'REF')
+% barplot2_ft(Q,timecell,[1:.1:30],'PFC')
+
+
+%error('stop')
+  
+  
+% % % % % % % % % % % % % % % % % % % % % % % % % CHECALE BIEN  
+% % % % % % % % % % % % % % % % % % % % % % % % % %   new_index=1;
+% % % % % % % % % % % % % % % % % % % % % % % % % % [TI,TN, cellx,cellr,to,tu]=win(carajo{:,:,level},veamos{level},sig1{new_index},sig2{new_index},ro);
+% % % % % % % % % % % % % % % % % % % % % % % % % % [z1,z4]=clean(cellx,cellr);
+% % % % % % % % % % % % % % % % % % % % % % % % % % [p3 ,p4]=eta2(z1,z4,ro,1000);
+
+%
+
+% 
+% 
+% new_index=3;
+% [TI,TN, cellx,cellr,to,tu]=win(carajo{:,:,level},veamos{level},sig1{new_index},sig2{new_index},ro);
+% [z2,z5]=clean(cellx,cellr);
+% [p3 ,p4]=eta2(z2,z5,ro,1000);
+% 
+% new_index=5;
+% [TI,TN, cellx,cellr,to,tu]=win(carajo{:,:,level},veamos{level},sig1{new_index},sig2{new_index},ro);
+% [z3,z6]=clean(cellx,cellr);
+% [p3 ,p4]=eta2(z3,z6,ro,1000);
+% 
+% new_index=7;
+% [TI,TN, cellx,cellr,to,tu]=win(carajo{:,:,level},veamos{level},sig1{new_index},sig2{new_index},ro);
+% [z7,z8]=clean(cellx,cellr);
+% [p3 ,p4]=eta2(z7,z8,ro,1000);
+
+%
+  
+%Use p and q and Q for Granger.
+  
+% % % % % % % % % % % % % % % % % % % % % % CHECALE BIEN P'AL GRANGER  
+% % % % % % % % % % % % % % % % % % % % % %   close all
+% % % % % % % % % % % % % % % % % % % % % %    q=cut(q);
+% % % % % % % % % % % % % % % % % % % % % %    p=cut(p);
+% % % % % % % % % % % % % % % % % % % % % %    %Q=cut(Q);
+% % % % % % % % % % % % % % % % % % % % % %    Q=cut(Q);
+% % % % % % % % % % % % % % % % % % % % % %   timecell=cut(timecell);
+% % % % % % % % % % % % % % % % % % % % % %   
+
+
+  
+%   [Fxy3, Fyx3]=BS(p,q);
+%   BS_thr(Fxy3,Fyx3,0.1);
+  
+
+  
+  %autotest(q,timecell,'Bandpassed',ro)
+
+ %%% CHECALE BIEN 
+% % % % % % % % % % % % % % % % % % %  gc(q,timecell,'Bandpassed',ro)
+% % % % % % % % % % % % % % % % % % %    
+% % % % % % % % % % % % % % % % % % %  string=strcat(num2str(ro),'_GC_','Monopolar','Bandpassed','.png');
+
+ 
+ 
+ %cd Nuevo
+%cd Spectrograms_Threshold_45
+%cd testGC
+% cd ARorder
+
+% % % % % % % % % CHECALE
+% % % % % % % % % fig=gcf;
+% % % % % % % % % fig.InvertHardcopy='off';
+% % % % % % % % % saveas(gcf,string)
+% % % % % % % % % %cd ..  
+% % % % % % % % %  close all
+ 
+ %autotest(p,timecell,'Widepassed',ro)
+
+ %CHECALE
+% % % % % % % % % % % % % % % % % % %  gc(p,timecell,'Widepass',ro)
+% % % % % % % % % % % % % % % % % % % 
+% % % % % % % % % % % % % % % % % % % string=strcat(num2str(ro),'_GC_','Monopolar','Widepass','.png');
+
+%cd Nuevo
+%cd Spectrograms_Threshold_45
+%cd testGC
+%cd ARorder
+
+%%%%%%%%%%%%%%%%%CHECALE
+% % % % % % % % % % % % % % % % fig=gcf;
+% % % % % % % % % % % % % % % % fig.InvertHardcopy='off';
+% % % % % % % % % % % % % % % % 
+% % % % % % % % % % % % % % % % saveas(gcf,string)
+% % % % % % % % % % % % % % % % %cd ..  
+% % % % % % % % % % % % % % % %  close all
+% % % % % % % % % % % % % % % %  % Envelope 
+% % % % % % % % % % % % % % % %  gc(Q,timecell,'Envelope',ro)
+% % % % % % % % % % % % % % % % 
+% % % % % % % % % % % % % % % % string=strcat(num2str(ro),'_GC_','Monopolar','Envelope','.png');
+% % % % % % % % % % % % % % % % %cd Nuevo
+% % % % % % % % % % % % % % % % %cd Spectrograms_Threshold_45
+% % % % % % % % % % % % % % % % %cd testGC
+% % % % % % % % % % % % % % % % %cd ARorder
+% % % % % % % % % % % % % % % % fig=gcf;
+% % % % % % % % % % % % % % % % fig.InvertHardcopy='off';
+% % % % % % % % % % % % % % % % 
+% % % % % % % % % % % % % % % % saveas(gcf,string)
+% % % % % % % % % % % % % % % % %cd ..  
+% % % % % % % % % % % % % % % %  close all
+ 
+ 
+%  T = cell2mat(q); 
+% [F]=mvgc_adapted(T,fn);
+% allscreen()
+% plot_granger(F,fn)
+% mtit('Monopolar','fontsize',14,'color',[1 0 0],'position',[.5 1 ])
+% mtit('Bandpassed','fontsize',14,'color',[1 0 0],'position',[.5 0.75 ])
+% mtit(strcat('(+/-',num2str(ro),'ms)'),'fontsize',14,'color',[1 0 0],'position',[.5 0.5 ])
+% 
+% 
+%  T = cell2mat(p); 
+% [F]=mvgc_adapted(T,fn);
+% allscreen()
+% plot_granger(F,fn)
+% mtit('Monopolar','fontsize',14,'color',[1 0 0],'position',[.5 1 ])
+% mtit('Wideband','fontsize',14,'color',[1 0 0],'position',[.5 0.75 ])
+% mtit(strcat('(+/-',num2str(ro),'ms)'),'fontsize',14,'color',[1 0 0],'position',[.5 0.5 ])
+% 
+%  T = cell2mat(Q); 
+% [F]=mvgc_adapted(T,fn);
+% allscreen()
+% plot_granger(F,fn)
+% mtit('Monopolar','fontsize',14,'color',[1 0 0],'position',[.5 1 ])
+% mtit('Envelope','fontsize',14,'color',[1 0 0],'position',[.5 0.75 ])
+% mtit(strcat('(+/-',num2str(ro),'ms)'),'fontsize',14,'color',[1 0 0],'position',[.5 0.5 ])
+
+end
+end
---
+++ read_ratdata27.m
@@ -0,0 +1,311 @@
+cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+
+% getdir=dir;
+% A_cell = struct2cell(getdir);
+% A_cell=A_cell(1,:);
+% A_cell=A_cell.';
+
+%%
+d = dir(cd);
+isub = [d(:).isdir]; %# returns logical vector
+nF = {d(isub).name}';
+nF(ismember(nF,{'.','..'})) = [];
+
+%%
+cd('/home/raleman/Documents/internship/27')
+%%
+for i=1:length(nF)
+    mkdir([nF{i}])
+end
+%%
+% d = dir(cd);
+% isub = [d(:).isdir]; %# returns logical vector
+% isub=not(isub);
+% nameFolds = {d(isub).name}';
+% % nameFolds(ismember(nameFolds,{'.','..'})) = [];
+
+%% Pick only the folders we are interested in 
+nFF=[
+      {'Rat27_for_II_2016-03-23_15-06-59'                    }
+    {'rat27_NL_baseline_2016-02-26_12-50-26'               }
+    {'rat27_for_2016-03-21_15-03-05'                       }
+    {'rat27_nl_base_2016-03-28_15-01-17'                   }
+    {'rat27_nl_base_III_2016-03-30_14-36-57'               }
+    %{'rat27_novelty_II_2016-04-13_14-37-58'                }  %NO .MAT files found. 
+    %{'rat27_novelty_II_2016-04-13_16-29-42'                } %No (complete).MAT files found.
+    {'rat27_novelty_I_2016-04-11_14-34-55'                 }
+    {'rat27_plusmaze_base_2016-03-14_14-52-48'             }
+    {'rat27_plusmaze_base_II_2016-03-24_14-10-08'          }
+%     {'rat27_plusmaze_dis_2016-03-10_14-35-18'              }
+%     {'rat27_plusmaze_dis_II_2016-03-16_14-36-07'           }
+%     {'rat27_plusmaze_dis_II_2016-03-18_14-46-24'           }
+%     {'rat27_plusmaze_jit_2016-03-08_14-46-31'              }
+%     {'rat27_plusmaze_jit_II_2016-03-16_15-02-27'           }
+%     {'rat27_plusmaze_swrd_qPCR_2016-04-15_14-28-41'        }
+%     {'rat27_watermaze_dis_morning_2016-04-06_10-18-36'     }
+%     {'rat27_watermaze_jitter_afternoon_2016-04-06_15-41-51'}  
+    ]
+%%
+%%
+for i=1:length(nFF)
+    mkdir([nFF{i}])
+end
+ %%
+ cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+
+ %%
+ Files=dir(fullfile(cd,'*.mat')) 
+ %% Get data 
+% g=cell(length(getdir),1)
+% g{:,1}=getdir.name;
+addpath('/home/raleman/Documents/MATLAB/analysis-tools-master'); %Open Ephys data loader. 
+addpath('/home/raleman/Documents/GitHub/CorticoHippocampal')
+%cd('/home/raleman/Documents/internship')
+cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+%%
+cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+
+iii=8
+%for iii=3:length(nFF)
+    cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+
+    cd(nFF{iii})
+    %clear all
+    %Find .mat files
+    Files=dir(fullfile(cd,'*.mat'));
+
+    if length(Files)~=2
+    
+    for ii=length(Files)-1:length(Files)
+    load(Files(ii).name)
+    end
+    
+    else
+        
+    
+    for ii=1:length(Files)
+    load(Files(ii).name)
+    end
+    
+    end
+    
+     'Channel 6'  
+%   
+%    [V6,~]=save_samples('100_CH22_merged.continuous',transitions);
+    [V6,~]=save_samples('100_CH22_2.continuous',transitions);
+
+    
+    cd('/home/raleman/Documents/internship/27')
+    cd(nFF{iii})
+    save('V6.mat','V6')
+    clear V6
+    
+    %
+    cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+    cd(nFF{iii})
+
+    'Channel 9'
+    
+%    [V9,~]=save_samples('100_CH9_merged.continuous',transitions);
+    [V9,~]=save_samples('100_CH9_2.continuous',transitions);
+
+    
+    cd('/home/raleman/Documents/internship/27')
+    cd(nFF{iii})
+    save('V9.mat','V9')
+    clear V9
+%
+    cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+    cd(nFF{iii})
+
+    'Channel 12'
+    
+%    [V12,tiempo]=save_samples('100_CH12_merged.continuous',transitions);
+    [V12,tiempo]=save_samples('100_CH12_2.continuous',transitions);
+
+    
+    cd('/home/raleman/Documents/internship/27')
+    cd(nFF{iii})
+    save('V12.mat','V12')
+    clear V12
+    
+cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+cd(nFF{iii})
+
+'Channel 17'
+
+%[V17,~]=save_samples('100_CH13_merged.continuous',transitions);
+[V17,~]=save_samples('100_CH13_2.continuous',transitions);
+
+
+cd('/home/raleman/Documents/internship/27')
+cd(nFF{iii})
+save('V17.mat','V17')
+clear V17
+
+%cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_27')
+
+
+
+%clear all
+clearvars -except nFF
+
+a=dir(fullfile(cd,'*.mat'))
+
+for ii=1:length(a)
+load(a(ii).name)
+end
+
+'Bipolar recordings'
+for j=1:length(V9)
+S9{j,1}=V9{j,1}-V6{j,1};
+S12{j,1}=V12{j,1}-V6{j,1};
+S17{j,1}=V17{j,1}-V6{j,1};
+end
+
+save('S9.mat','S9')
+save('S12.mat','S12')
+save('S17.mat','S17')
+
+
+% end
+% END OF CODE
+
+%% Reading data
+cd('/home/raleman/Documents/internship/26')
+i=1;
+cd(nFF{i})
+
+
+S17=load('S17.mat');
+S17=S17.S17;
+
+S12=load('S12.mat');
+S12=S12.S12;
+
+S9=load('S9.mat');
+S9=S9.S9;
+
+V17=load('V17.mat');
+V17=V17.V17;
+
+V12=load('V12.mat');
+V12=V12.V12;
+
+V9=load('V9.mat');
+V9=V9.V9;
+
+V6=load('V6.mat');
+V6=V6.V6;
+%%
+%% Band pass design 
+fn=1000; % New sampling frequency. 
+Wn1=[100/(fn/2) 300/(fn/2)]; % Cutoff=500 Hz
+[b1,a1] = butter(3,Wn1,'bandpass'); %Filter coefficients for LPF
+% Bandpass filter 
+fn=1000;
+
+Mono6=cell(length(S9),1);
+Mono9=cell(length(S9),1);
+Mono12=cell(length(S9),1);
+Mono17=cell(length(S9),1);
+
+Bip9=cell(length(S9),1);
+Bip12=cell(length(S9),1);
+Bip17=cell(length(S9),1);
+
+
+for i=1:length(S9)
+    
+Bip9{i}=filtfilt(b1,a1,S9{i});    
+Bip12{i}=filtfilt(b1,a1,S12{i});
+Bip17{i}=filtfilt(b1,a1,S17{i});
+
+Mono6{i}=filtfilt(b1,a1,V6{i});
+Mono9{i}=filtfilt(b1,a1,V9{i});
+Mono12{i}=filtfilt(b1,a1,V12{i});
+Mono17{i}=filtfilt(b1,a1,V17{i});
+
+end
+
+%%
+s17=nan(length(S9),1);
+swr17=cell(length(S9),3);
+%thr=140;
+thr=200;
+%thr=180;
+
+
+for i=1:length(S9)
+    
+signal=Bip17{i}*(1/0.195);
+signal2=Mono17{i}*(1/0.195);
+
+ti=(0:length(signal)-1)*(1/fn); %IN SECONDS
+[S, E, M] = findRipplesLisa(signal, ti.', thr , (thr)*(1/3), []);
+s17(i)=length(M);
+swr17{i,1}=S;
+swr17{i,2}=E;
+swr17{i,3}=M;
+
+ti=(0:length(signal2)-1)*(1/fn); %IN SECONDS
+[S2, E2, M2] = findRipplesLisa(signal2, ti.', thr , (thr)*(1/3), []);
+s217(i)=length(M2);
+swr217{i,1}=S2;
+swr217{i,2}=E2;
+swr217{i,3}=M2;
+
+
+i/length(S9)
+end
+
+% Windowing
+veamos=find(s17~=0);  %Epochs with ripples detected
+carajo=swr17(veamos,:);
+
+%Proceed to rearrange.m
+%  Windowing using Monopolar
+
+veamos2=find(s217~=0);  %Epochs with ripples detected
+carajo2=swr217(veamos2,:);
+
+
+%%
+cd('/media/raleman/My Book/SWRDisruptionPlusMaze/rat_26')
+cd(nFF{1})
+
+[V17,tiempo]=save_samples('100_CH17_merged.continuous',transitions);
+
+cd('/home/raleman/Documents/internship/26')
+cd(nFF{1})
+save('V17.mat','V17')
+clear V17
+%%
+clear all
+a=dir(fullfile(cd,'*.mat'))
+
+for ii=1:length(a)
+load(a(ii).name)
+end
+
+for j=1:length(V9)
+S9{j,1}=V9{j,1}-V6{j,1};
+S12{j,1}=V12{j,1}-V6{j,1};
+S17{j,1}=V17{j,1}-V6{j,1};
+end
+
+save('S9.mat','S9')
+save('S12.mat','S12')
+save('S17.mat','S17')
+
+
+%%
+%Reference
+[data6m, ~, ~] = load_open_ephys_data_faster('100_CH6_merged.continuous');
+%%
+[C6,tiempo]=reduce_data(data6m,transitions);
+clear data6m
+[V6]=downsampling(C6);
+clear C6
+
+
