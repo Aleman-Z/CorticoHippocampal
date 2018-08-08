cefb4382e4247ef4e9a821a56a490cc8838681ff
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-08-08 14:01:29 +0200

    addtopath

--- Figures/spectrogram_without_normalization_mergebaselines.m
+++ Figures/spectrogram_without_normalization_mergebaselines.m
@@ -17,6 +17,7 @@ addpath('/home/raleman/Documents/internship')
 else
 addpath('D:\internship\analysis-tools-master'); %Open Ephys data loader.
 addpath(genpath('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+addpath(genpath('C:\Users\addri\Documents\GitHub\ADRITOOLS'))
 %addpath(('C:\Users\addri\Documents\internship\CorticoHippocampal'))
    
 end
