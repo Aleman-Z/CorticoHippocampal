8a4448062238e4d8c4d5969d40edcebbe7e50223
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-08-11 13:50:40 +0200

    1000 ripples

--- select_rip.m
+++ select_rip.m
@@ -9,7 +9,7 @@ function [ran]=select_rip(p,FiveHun)
     %[B I]= maxk(max(av.'),1000); %Look for the 1000 ripples with highest amplitude. THIS SHOULD BE MEAN instead of Max since some peaks go downwards and no upwards. 
 
     [ach]=max(av.');
-    achinga=sort(ach,'descend');
+    [achinga]=sort(ach,'descend');
     if FiveHun==1 || FiveHun==2
         if length(achinga)>1000
         achinga=achinga(1:1000);
@@ -22,9 +22,15 @@ function [ran]=select_rip(p,FiveHun)
     I(hh)= find(ach==achinga(hh),1,'first');
     end
 
-
-
-    [ajal ind]=unique(B); %Repeated ripples, which are very close to each other. 
+ if FiveHun==1
+    [ajal, ind]=unique(B); %Repeated ripples, which are very close to each other. 
+ end 
+ 
+ if FiveHun ==2
+    ajal=B;
+    
+ end
+    
 if FiveHun==1
     if length(ajal)>500
     ajal=ajal(end-499:end);
@@ -35,9 +41,11 @@ end
     % ajal=ajal(end-999:end);
     % ind=ind(end-999:end);
     % end
-
+if FiveHun==1
     dex=I(ind);
-    % Files=dir(fullfile(cd,'*.mat'));
+else
+    dex=I;
+end  % Files=dir(fullfile(cd,'*.mat'));
 
 
     % ran=I.'; % Select ripples with highest magnitudes. 
