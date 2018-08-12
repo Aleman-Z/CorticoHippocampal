a1a772ecf2181bc446fc89537b6d396eb0c7e01c
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-08-12 22:18:05 +0200

    find sessions with no thr

---
+++ Threshold_Selection/find_sessions_withouth_thr.m
@@ -0,0 +1,145 @@
+acer=0;
+meth=3;
+Rat=26;
+
+%%
+if acer==0
+addpath('/home/raleman/Documents/MATLAB/analysis-tools-master'); %Open Ephys data loader. 
+addpath(genpath('/home/raleman/Documents/GitHub/CorticoHippocampal'))
+addpath(genpath('/home/raleman/Documents/GitHub/ADRITOOLS'))
+addpath('/home/raleman/Documents/internship')
+else
+addpath('D:\internship\analysis-tools-master'); %Open Ephys data loader.
+addpath(genpath('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+addpath(genpath('C:\Users\addri\Documents\GitHub\ADRITOOLS'))
+%addpath(('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+   
+end
+%%
+if Rat==26
+nFF=[
+%    {'rat26_Base_II_2016-03-24'                         }
+%    {'rat26_Base_II_2016-03-24_09-47-13'                }
+%    {'rat26_Base_II_2016-03-24_12-55-58'                }
+%    {'rat26_Base_II_2016-03-24_12-57-57'                }
+    
+   
+%   {'rat26_nl_base_III_2016-03-30_10-32-57'            }
+    {'rat26_nl_base_II_2016-03-28_10-40-19'             }
+     {'rat26_nl_baseline2016-03-01_11-01-55'             }
+    {'rat26_plusmaze_base_2016-03-08_10-24-41'}
+    
+    
+    
+    {'rat26_novelty_I_2016-04-12_10-05-55'          }
+%     {'rat26_novelty_II_2016-04-13_10-23-29'             }
+    {'rat26_for_2016-03-21_10-38-54'                    }
+%     {'rat26_for_II_2016-03-23_10-49-50'                 }
+    
+    ];
+
+end
+
+if Rat==27
+nFF=[
+     {'rat27_nl_base_2016-03-28_15-01-17'                   }
+     {'rat27_NL_baseline_2016-02-26_12-50-26'               }
+   % {'rat27_nl_base_III_2016-03-30_14-36-57'               }
+    
+    {'rat27_plusmaze_base_2016-03-14_14-52-48'             }
+ %   {'rat27_plusmaze_base_II_2016-03-24_14-10-08'          }
+    {'rat27_novelty_I_2016-04-11_14-34-55'                 } 
+    {'rat27_for_2016-03-21_15-03-05'                       }
+    %{'Rat27_for_II_2016-03-23_15-06-59'                    }
+    
+    %{'rat27_novelty_II_2016-04-13_14-37-58'                }  %NO .MAT files found. 
+    %{'rat27_novelty_II_2016-04-13_16-29-42'                } %No (complete).MAT files found.
+       ];
+      
+end
+
+
+%% Go to main directory
+if acer==0
+    cd(strcat('/home/raleman/Documents/internship/',num2str(Rat)))
+    addpath /home/raleman/Documents/internship/fieldtrip-master/
+    InitFieldtrip()
+
+    cd(strcat('/home/raleman/Documents/internship/',num2str(Rat)))
+    clc
+else
+    cd(strcat('D:\internship\',num2str(Rat)))
+    addpath D:\internship\fieldtrip-master
+    InitFieldtrip()
+
+    % cd(strcat('/home/raleman/Documents/internship/',num2str(Rat)))
+    cd(strcat('D:\internship\',num2str(Rat)))
+    clc
+end
+%%
+% % % % 
+% % % % %% Select experiment to perform. 
+% % % % % inter=1;
+% % % % %Select length of window in seconds:
+% % % % if dura==1
+% % % % ro=[1200];
+% % % % else
+% % % % ro=[10200];    
+% % % % end
+% % % % % coher=0;
+% % % % % selectripples=1;
+% % % % notch=0;
+% % % % nrem=3;
+% % % % myColorMap = jet(8);
+% % % % % Score=1;
+% % % % %%
+% % % % 
+% % % % %Make labels
+% % % % label1=cell(7,1);
+% % % % label1{1}='Hippo';
+% % % % label1{2}='Hippo';
+% % % % label1{3}='Parietal';
+% % % % label1{4}='Parietal';
+% % % % label1{5}='PFC';
+% % % % label1{6}='PFC';
+% % % % label1{7}='Reference';
+% % % % 
+% % % % label2=cell(7,1);
+% % % % label2{1}='Monopolar';
+% % % % label2{2}='Bipolar';
+% % % % label2{3}='Monopolar';
+% % % % label2{4}='Bipolar';
+% % % % label2{5}='Monopolar';
+% % % % label2{6}='Bipolar';
+% % % % label2{7}='Monopolar';
+% % % % 
+% % % % %%
+% % % % 
+% % % %  
+% % % % for iii=2:length(nFF) %Should start with 2!
+% % % % %xo
+% % % % 
+% % % % 
+% % % % if acer==0
+% % % %     cd(strcat('/home/raleman/Documents/internship/',num2str(Rat)))
+% % % % else
+% % % %     cd(strcat('D:\internship\',num2str(Rat)))
+% % % % end
+% % % % 
+% % % % cd(nFF{iii})
+% % % % lepoch=2;
+% % % % 
+% % % %  level=1;
+% % % %  
+% % % % xo
+% % % % if meth==3
+% % % % chtm=load('vq_loop2.mat');
+% % % % chtm=chtm.vq;
+% % % %     [sig1,sig2,ripple,carajo,veamos,RipFreq2,timeasleep,~]=nrem_fixed_thr_Vfiles(chtm,notch);
+% % % % CHTM=[chtm chtm];
+% % % % end
+% % % % 
+% % % % end
+% % % % 
+% % % % end
+% % % % 
