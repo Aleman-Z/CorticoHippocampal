7068e9d9ff15afdced924c4602b6226822a0f5fc
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-08-13 00:35:30 +0200

    testing loop and update

---
+++ Ideas_testing/testing_loop.m
@@ -0,0 +1,305 @@
+acer=1;
+% rat24base=1;
+DUR{1}='1sec';
+DUR{2}='10sec';
+Block{1}='complete';
+Block{2}='block1';
+Block{3}='block2';
+mergebaseline=0;
+FiveHun=2; % Options: 0 all, 1 current, 2 1000?
+%meth=1;
+
+%%
+if acer==0
+addpath('/home/raleman/Documents/MATLAB/analysis-tools-master'); %Open Ephys data loader. 
+addpath(genpath('/home/raleman/Documents/GitHub/CorticoHippocampal'))
+addpath(genpath('/home/raleman/Documents/GitHub/ADRITOOLS'))
+addpath('/home/raleman/Documents/internship')
+else
+addpath('D:\internship\analysis-tools-master'); %Open Ephys data loader.
+addpath(genpath('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+addpath(genpath('C:\Users\addri\Documents\GitHub\ADRITOOLS'))
+%addpath(('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+   
+end
+%%
+%Rat=26;
+for meth=1:2
+for RAT=2:2
+  base=1;  
+% for base=1:2 %Baseline numeration.     
+while base<=2
+%for rat24base=1:2
+ 
+%   if RAT~=3 && rat24base==2
+%       break
+%   end
+
+%for dura=1:1 %Starts with 1
+    
+rats=[26 27 24 21];
+Rat=rats(RAT);    
+    
+% for Rat=26:26
+if Rat==26
+nFF=[
+%    {'rat26_Base_II_2016-03-24'                         }
+%    {'rat26_Base_II_2016-03-24_09-47-13'                }
+%    {'rat26_Base_II_2016-03-24_12-55-58'                }
+%    {'rat26_Base_II_2016-03-24_12-57-57'                }
+    
+   
+%   {'rat26_nl_base_III_2016-03-30_10-32-57'            }
+ %    {'rat26_nl_base_II_2016-03-28_10-40-19'             }
+     {'rat26_nl_baseline2016-03-01_11-01-55'             }
+    {'rat26_plusmaze_base_2016-03-08_10-24-41'}
+    
+    
+    
+    {'rat26_novelty_I_2016-04-12_10-05-55'          }
+%     {'rat26_novelty_II_2016-04-13_10-23-29'             }
+    {'rat26_for_2016-03-21_10-38-54'                    }
+%     {'rat26_for_II_2016-03-23_10-49-50'                 }
+    
+    ];
+
+if strcmp(nFF{1},'rat26_nl_baseline2016-03-01_11-01-55')
+NFF=[ {'rat26_nl_base_II_2016-03-28_10-40-19'             }];    
+end
+if strcmp(nFF{1},'rat26_nl_base_II_2016-03-28_10-40-19')
+NFF=[ {'rat26_nl_baseline2016-03-01_11-01-55'             }];
+end
+
+
+% labelconditions=[
+%     {'Baseline_1' 
+%      'Baseline_2'}
+%      'Baseline_3'
+%      'PlusMaze'
+%      'Novelty_1'
+%      'Novelty_2'
+%      'Foraging_1'
+%      'Foraging_2'
+%     ];
+
+labelconditions=[
+    { 
+    
+    'Baseline'}
+%     'Baseline_2'
+%     'Baseline_3'
+     'PlusMaze'    
+     'Novelty'
+%      'Novelty_2'
+     'Foraging'
+%      'Foraging_2'
+    ];
+
+
+end
+if Rat==27
+nFF=[
+     {'rat27_nl_base_2016-03-28_15-01-17'                   } %Baseline 2: Use this one. 
+   % {'rat27_NL_baseline_2016-02-26_12-50-26'               }
+   % {'rat27_nl_base_III_2016-03-30_14-36-57'               }
+    
+   {'rat27_plusmaze_base_2016-03-14_14-52-48'             }
+   %{'rat27_plusmaze_base_II_2016-03-24_14-10-08'          }
+    {'rat27_novelty_I_2016-04-11_14-34-55'                 } 
+    {'rat27_for_2016-03-21_15-03-05'                       }
+    %{'Rat27_for_II_2016-03-23_15-06-59'                    }
+    
+    %{'rat27_novelty_II_2016-04-13_14-37-58'                }  %NO .MAT files found. 
+    %{'rat27_novelty_II_2016-04-13_16-29-42'                } %No (complete).MAT files found.
+   
+  
+%     {'rat27_plusmaze_dis_2016-03-10_14-35-18'              }
+%     {'rat27_plusmaze_dis_II_2016-03-16_14-36-07'           }
+%     {'rat27_plusmaze_dis_II_2016-03-18_14-46-24'           }
+%     {'rat27_plusmaze_jit_2016-03-08_14-46-31'              }
+%     {'rat27_plusmaze_jit_II_2016-03-16_15-02-27'           }
+%     {'rat27_plusmaze_swrd_qPCR_2016-04-15_14-28-41'        }
+%     {'rat27_watermaze_dis_morning_2016-04-06_10-18-36'     }
+%     {'rat27_watermaze_jitter_afternoon_2016-04-06_15-41-51'}  
+    ];
+%NFF=[{'rat27_NL_baseline_2016-02-26_12-50-26'               }];
+if strcmp(nFF{1},'rat27_NL_baseline_2016-02-26_12-50-26')
+NFF=[ {  'rat27_nl_base_2016-03-28_15-01-17'           }];    
+end
+if strcmp(nFF{1}, 'rat27_nl_base_2016-03-28_15-01-17')
+NFF=[ {'rat27_NL_baseline_2016-02-26_12-50-26'   }];
+end
+
+
+labelconditions=[
+    { 
+    'Baseline'}
+%     'Baseline_2'
+%     'Baseline_3'
+    'PlusMaze'
+%     'PlusMaze_2'
+    'Novelty'
+    'Foraging'
+    
+  %   'Foraging_2'
+     
+    
+    
+     
+    ];
+
+    
+end
+
+if Rat==21
+ 
+ nFF=[  
+    {'2015-11-27_13-50-07 5h baseline'             }
+    {'rat21 baselin2015-12-11_12-52-58'            }
+    {'rat21_learningbaseline2_2015-12-10_15-24-17' }
+    {'rat21with45minlearning_2015-12-02_14-25-12'  }
+    %{'rat21t_maze_2015-12-14_13-29-07'             }
+    {'rat21 post t-maze 2015-12-14_13-30-52'       }
+    
+];
+
+%%
+labelconditions=[
+    {    
+     'Learning Baseline'
+                }
+     
+     '45minLearning'
+     'Novelty_2'
+     't-maze'
+     'Post t-maze'
+    ];
+    
+end
+
+if Rat==24
+nFF=[  
+    {'Baseline1'}
+%     {'Baseline2'}
+%     {'Baseline3'}
+%     {'Baseline4'}
+    {'Plusmaze1'}
+%     {'Plusmaze2'}
+   {'Novelty1'}
+   {'Foraging1'}
+     
+]; 
+if  rat24base==2
+  nFF{1,:}='Baseline2'; 
+end
+
+%labelconditions=nFF;
+labelconditions=[
+    { 
+    'Baseline'}
+%     'Baseline_2'
+%     'Baseline_3'
+    'PlusMaze'
+%     'PlusMaze_2'
+    'Novelty'
+    'Foraging'
+    
+  %   'Foraging_2'
+     
+    
+    
+     
+    ];
+
+
+
+end
+
+%% Check if experiment has been run before.
+if acer==0
+    cd(strcat('/home/raleman/Dropbox/Figures/Figure3/',num2str(Rat)))
+else
+      %cd(strcat('C:\Users\Welt Meister\Dropbox\Figures\Figure2\',num2str(Rat)))   
+      cd(strcat('C:\Users\addri\Dropbox\Figures\Figure3\',num2str(Rat)))   
+end
+
+if Rat==24
+    cd(nFF{1})
+end
+% 
+% if dura==2
+%     cd('10sec')
+% end
+%xo
+
+%Ignore this for a moment
+FolderRip=[{'all_ripples'} {'500'} {'1000'}];
+if Rat==26
+Base=[{'Baseline1'} {'Baseline2'}];
+end
+if Rat==27 
+Base=[{'Baseline2'} {'Baseline1'}];% We run Baseline 2 first, cause it is the one we prefer.
+end
+
+if meth==1
+Folder=strcat(Base{base},'_',FolderRip{FiveHun+1});
+else
+Method=[{'Method2' 'Method3'}];
+Folder=strcat(Base{base},'_',FolderRip{FiveHun+1},'_',Method{meth-1});    
+end
+
+if exist(Folder)==7 && base==1
+base=base+1;
+end
+%%
+% Use other baseline, beware when using mergebaseline
+if base==2
+    nFF{1}=NFF{1};
+end
+
+if base==3
+    break
+end
+
+
+
+%%
+%save in right folder
+list = dir();
+list([list.isdir]) = [];
+list={list.name};
+FolderRip=[{'all_ripples'} {'500'} {'1000'}];
+if Rat==26
+Base=[{'Baseline1'} {'Baseline2'}];
+end
+if Rat==27 
+Base=[{'Baseline2'} {'Baseline1'}];% We run Baseline 2 first, cause it is the one we prefer.
+end
+
+if meth==1
+folder=strcat(Base{base},'_',FolderRip{FiveHun+1});
+else
+Method=[{'Method2' 'Method3'}];
+folder=strcat(Base{base},'_',FolderRip{FiveHun+1},'_',Method{meth-1});    
+end
+
+base
+folder
+pause(3)
+% if exist(folder)~=7
+% (mkdir(folder))
+% end
+
+
+clearvars -except RAT acer DUR Block mergebaseline FiveHun meth block_time base
+if base>=2
+    break
+end
+base=2;
+end
+base=1;
+
+end
+end
+%end
+%end
