68ea3311bd92f80b9661ab8069965a4104371a76
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-08-30 10:23:08 +0200

    U

--- Ideas_testing/test_average_ripfreq.m
+++ Ideas_testing/test_average_ripfreq.m
@@ -8,14 +8,23 @@ for k=1:length(q)
 %  semilogy(f,(pxx))
  vq1 = interp1(f,pxx,xq,'PCHIP');
  VQ(k,:)=vq1;
+ U(op)=interp1(VQ(k,:),xq,max(VQ(k,:)));
  %%
 %  semilogy(f,(pxx))
 %  hold on
 %  semilogy(xq,(vq1))
  k
 end
+
 %%
 af=mean(VQ,1);
 plot(xq,af)
 
 AV=interp1(af,xq,max(af));
+%%
+% U=interp1(VQ,(repmat(xq,2000,1)),max(VQ.'));
+for op=1:length(q)
+  U(op)=interp1(VQ(op,:),xq,max(VQ(op,:)));
+  op
+end
+
