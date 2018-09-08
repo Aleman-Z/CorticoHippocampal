80e12d0d341de0cf1d765971aeb4f6ac991b4a8a
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-08 20:44:25 +0200

    add unique

--- max_outlier.m
+++ max_outlier.m
@@ -6,6 +6,19 @@ R=reshape(P,[length(p) size(p{1},2)]);
 R=max(R.');
 
 H=~isoutlier(R);
+
+%Look for unique:
+Vec=1:length(p);
+vec=1:length(p);
+vec=vec.*H;
+vec=vec(vec~=0); %Index of no outliers. 
+
+%Get index of unique
+[~,a2]=unique(R,'first'); 
+chidos=(intersect(vec,a2)); %Unique and non-outlier. 
+
+H=ismember(Vec,chidos); %Index of elements in chidos. 
+
 % H=~outlier(R,10);
 % pp=p(H);
 end
