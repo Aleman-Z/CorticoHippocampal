310a38d0b2e8b856e9298ef3b4535c509f5a7bd0
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-09 17:34:07 +0200

    spindle_normalization

--- baseline_norm.m
+++ baseline_norm.m
@@ -3,15 +3,17 @@ function [achis]=baseline_norm(freq1,w)
 
 %%Average trials
 TF=freq1.powspctrm;
+
 TF=squeeze(mean(TF,1));
 TF=squeeze(TF(w,:,:));
 TFt=freq1.time;
 
 
 tind=([-1 -0.5]);
-ind1=find(TFt==tind(1));
-ind2=find(TFt==tind(2));
-
+% ind1=find(TFt==tind(1));
+% ind2=find(TFt==tind(2));
+ind1=find((ismembertol(TFt,tind(1))));
+ind2=find((ismembertol(TFt,tind(2))));
 
 FF=mean(squeeze(TF(:,ind1:ind2)),2); %baseline
 
