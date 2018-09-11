1af3a0f12de98660405fe0e7d9adc48e4e99abd9
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-11 16:54:19 +0200

    Update LL_clean.m

--- LL_clean.m
+++ LL_clean.m
@@ -1,6 +1,6 @@
 function Avero=LL_clean(LL)
     for v=1:size(LL,1)
-        for j=1:size(LL,1)
+        for j=1:size(LL,2)
             avero=cell2mat(LL(v,j));
             Avero(v,j)={avero(~isoutlier(avero))};
         end
