15e5e86373528c5c1c28cca65ce67133f48e2ba1
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-19 13:06:05 +0200

    upload_granger_new_stats

---
+++ Granger/granger_2D_stats_conditions.m
@@ -0,0 +1,92 @@
+function granger_2D_stats_conditions(g1,g1_f,labelconditions,freqrange)
+allscreen()
+F= [1 2; 1 3; 2 3] ;
+
+lab=cell(12,1);
+
+lab{1}={'Baseline vs Foraging' 'Baseline vs Novelty' 'Baseline vs Plusmaze'}.';
+% lab{2}='Baseline vs Novelty';
+% lab{3}='Baseline vs Plusmaze';
+
+lab{4}='Foraging vs Baseline';
+lab{5}='Foraging vs Novelty';
+lab{6}='Foraging vs Plusmaze';
+
+lab{7}='Novelty vs Baseline';
+lab{8}='Novelty vs Foraging';
+lab{9}='Novelty vs Plusmaze';
+
+lab{10}='Plusmaze vs Base';
+lab{11}='Plusmaze vs Foraging';
+lab{12}='Plusmaze vs Novelty';
+
+%%
+f=F(1,:);
+
+for j=1:1
+I=subplot(2,2,j)
+turnim=[squeeze(g1{1}(f(1),f(2),:)).'; squeeze(g1{2}(f(1),f(2),:)).' ;squeeze(g1{3}(f(1),f(2),:)).' ;squeeze(g1{4}(f(1),f(2),:)).'];
+% turnim2=[squeeze(g1{1}(f(2),f(1),:)).'; squeeze(g1{2}(f(2),f(1),:)).' ;squeeze(g1{3}(f(2),f(1),:)).' ;squeeze(g1{4}(f(2),f(1),:)).'];
+
+
+zmap(1,:)=stats_high2((turnim(1,:)),(turnim(2,:)));
+zmap(2,:)=stats_high2((turnim(1,:)),(turnim(3,:)));
+zmap(3,:)=stats_high2((turnim(1,:)),(turnim(4,:)));
+
+imagesc(zmap); colorbar(); colormap(jet(256))
+%%
+zmap(1,:)=stats_high2((turnim(2,:)),(turnim(1,:)));
+zmap(2,:)=stats_high2((turnim(3,:)),(turnim(1,:)));
+zmap(3,:)=stats_high2((turnim(4,:)),(turnim(1,:)));
+
+zmap(zmap == 0) = NaN;
+J=imagesc(g1_f,[1:4],zmap); colorbar(); colormap(jet(256))
+set(J,'AlphaData',~isnan(zmap))
+xlabel('Frequency (Hz)')
+I=gca;
+I.YTickLabel=lab{1};
+
+
+%Stats
+I=subplot(3,3,3*j)
+J=imagesc(g1_f,[1:4],zmap)
+colormap(jet(256))
+ colorbar()
+xlabel('Frequency (Hz)')
+% I=gca;
+% I.YTickLabel=[{} labelconditions{1} {} labelconditions{2} {} labelconditions{3} {} labelconditions{4} {}];
+% I.YTickLabel=[{' '} labelconditions{1} {' '} labelconditions{2} {' '} labelconditions{3} {' '} labelconditions{4} {' '} ];
+I.YTickLabel=labelconditions;
+%xlim auto
+
+%
+%set(gca,'xlim',xlim,'ydir','no')
+zmap(zmap == 0) = NaN;
+%aj=find(~isnan(zmap));
+dale=0;
+for ac=1:4
+aj=sum(~isnan(zmap(ac,:)));
+if aj~=0
+       dale1=max(find(~isnan(zmap(ac,:))));
+       dale1=abs(dale1);
+       if dale1>dale
+           dale=dale1;
+       end
+end
+end
+set(J,'AlphaData',~isnan(zmap))
+
+% c=narrow_colorbar()
+%  c.YLim=[-max(abs(c.YLim)) max(abs(c.YLim))];
+% caxis([-max(abs(c.YLim)) max(abs(c.YLim))])
+% c=narrow_colorbar()
+if dale==0 
+xlim(freqrange)
+else
+%xlim([0 dale*2+1])
+xlim([0 g1_f(dale)+2])
+end
+% xlim(freqrange)
+title(lab{3*j})
+end
+end
