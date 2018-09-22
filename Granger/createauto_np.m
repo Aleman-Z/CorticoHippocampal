4dc22fc6ade98d394c5ecf9fd4400fac77eaf8b6
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-22 15:48:41 +0200

    createauto_np

---
+++ Granger/createauto_np.m
@@ -0,0 +1,14 @@
+function [granger]=createauto_np(data1)
+    cfg           = [];
+    cfg.method    = 'mtmfft';
+    cfg.taper     = 'dpss';
+    cfg.output    = 'fourier';
+    cfg.tapsmofrq = 2;
+    cfg.pad = 10;
+    cfg.foi=[0:1:500];
+    freq          = ft_freqanalysis(cfg, data1);
+
+    cfg           = [];
+    cfg.method    = 'granger';
+    granger = ft_connectivityanalysis(cfg, freq);
+end
