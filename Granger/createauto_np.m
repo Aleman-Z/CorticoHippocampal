f1ca6ff4cc0d29ce7e4c788203f936448f2201d5
Adrian Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-22 17:17:34 +0200

    Update createauto_np.m

--- Granger/createauto_np.m
+++ Granger/createauto_np.m
@@ -1,11 +1,12 @@
-function [granger]=createauto_np(data1)
+function [granger]=createauto_np(data1,freqrange)
     cfg           = [];
     cfg.method    = 'mtmfft';
     cfg.taper     = 'dpss';
     cfg.output    = 'fourier';
     cfg.tapsmofrq = 2;
     cfg.pad = 10;
-    cfg.foi=[0:1:500];
+    cfg.foi=freqrange;
+    %[0:1:500]
     freq          = ft_freqanalysis(cfg, data1);
 
     cfg           = [];
