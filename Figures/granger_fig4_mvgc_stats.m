f9a7650cb0cabf37a7a6dff019db21b7182c0bfa
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-09-25 00:35:54 +0200

    update gcausality

--- Figures/granger_fig4_mvgc_stats.m
+++ Figures/granger_fig4_mvgc_stats.m
@@ -367,17 +367,73 @@ xo
 
 %MVGC
 %%
+% FOR FP
 %Baseline
 GRC1=FP{1};
 % GRC1(isnan(GRC1))=0;
-gr1=abs(squeeze(GRC1(3,1,:,:)));
-ggr1=gr1-mean(gr1,1);
-
 %Plusmaze
 GRC2=FP{4};
-% GRC2(isnan(GRC2))=0;
-gr2=abs(squeeze(GRC2(3,1,:,:)));
-ggr2=gr2-mean(gr2,1);
+n_rip=size(GRC2,4); %EQUALIZES NUMBER OF RIPPLES. 
+GRC1=GRC1(:,:,:,1:n_rip);
+
+
+
+%Select combinations. 
+F= [1 2; 1 3; 2 3] ;
+wb=waitbar(0,'wait')
+
+ for j=1:3
+    waitbar(j/3,wb,'wait')
+      f=F(j,:);
+    %Baseline  
+    %First direction  
+    gr1=abs(squeeze(GRC1(f(1),f(2),:,:)));
+    ggr1=gr1-mean(gr1,1);
+    
+    %Plusmaze
+    % GRC2(isnan(GRC2))=0;
+    gr2=abs(squeeze(GRC2(f(1),f(2),:,:)));
+    ggr2=gr2-mean(gr2,1);
+
+        for ef=1:501
+          [p(ef)]=permutationTest(gr2(ef,:),gr1(ef,:),1000);
+            ef
+        end
+    
+        P=p; %P value
+        PP(P<0.05)=1;
+        PP(P>=0.05)=0; %Binarized P value. 
+    
+    Psaver{j}=P;
+    PPsaver{j}=PP;
+    
+    %Opposite direction
+    Xgr1=abs(squeeze(GRC1(f(2),f(1),:,:)));
+    Xggr1=Xgr1-mean(Xgr1,1);
+        
+    Xgr2=abs(squeeze(GRC2(f(2),f(1),:,:)));
+    Xggr2=Xgr2-mean(Xgr2,1);
+        
+    
+    
+        for ef=1:501
+          [p(ef)]=permutationTest(Xgr2(ef,:),Xgr1(ef,:),1000);
+            ef
+        end
+    
+        P=p; %P value
+        PP(P<0.05)=1;
+        PP(P>=0.05)=0; %Binarized P value. 
+
+        XPsaver{j}=P;
+        XPPsaver{j}=PP;
+    
+ end
+close(wb)
+
+
+%Stop it here.
+xo
 %%
 pp=figure()
 plot(mean(gr1,2))
