6a94e452d5fa89e63e3788cd6b86c25ae43b446a
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-10-01 19:04:11 +0200

    coherence

---
+++ Ideas_testing/coherence_test.m
@@ -0,0 +1,23 @@
+ fs = 1000; 
+ tstep = 2;%samples
+
+ spec_win = fs/2/2/2; 
+ spec_win=100;
+%  spec_win = fs/2; 
+
+ %nfft = fs*30; 
+nfft = 500; 
+
+ 
+  x=p{1}(1,:);
+  y=p{1}(2,:);
+ 
+  jo=tfcohf2(x,y,nfft,spec_win,tstep,fs);
+  %%
+  av1=linspace(0,500,size(jo,1));
+  av2=linspace(-1.2,1.2,size(jo,2));
+  imagesc(av2,av1,abs(jo))
+  colormap(jet(256))
+  ylim([0 300])
+  xlim([-1 1])
+  colorbar()
