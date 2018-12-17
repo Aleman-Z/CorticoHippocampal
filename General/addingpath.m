73c13969807952625570e5c3d1670b9aebbc07bf
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2018-12-17 17:03:28 +0100

    addingpath

---
+++ General/addingpath.m
@@ -0,0 +1,15 @@
+
+function addingpath(cero)
+    if acer==cero
+    addpath('/home/raleman/Documents/MATLAB/analysis-tools-master'); %Open Ephys data loader. 
+    addpath(genpath('/home/raleman/Documents/GitHub/CorticoHippocampal'))
+    addpath(genpath('/home/raleman/Documents/GitHub/ADRITOOLS'))
+    addpath('/home/raleman/Documents/internship')
+    else
+    addpath('D:\internship\analysis-tools-master'); %Open Ephys data loader.
+    addpath(genpath('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+    addpath(genpath('C:\Users\addri\Documents\GitHub\ADRITOOLS'))
+    %addpath(('C:\Users\addri\Documents\internship\CorticoHippocampal'))
+    addpath(genpath('C:\Users\addri\Documents\MATLAB\mvgc_v1.0'))   
+    end
+end
