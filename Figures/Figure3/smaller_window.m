32bfe56cea879a62548f9a96ebe04d98f57d1784
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2019-03-06 08:46:27 +0100

    smaller_window

--- Figures/Figure3/smaller_window.m
+++ Figures/Figure3/smaller_window.m
@@ -1,10 +1,14 @@
 %freq3
-function [zmin100,zmax100]=smaller_window(freq3)
+function [mdam, sdam]=smaller_window(freq3,w)
 
-    FG3=freq3;
-    FG3.time=[-.05:.001:.05];
-    FG3.powspctrm=freq3.powspctrm(:,:,:,1+50:end-50);
+dam=((squeeze(mean(squeeze(freq3.powspctrm(:,w,:,1+50:end-50)),1)))); %Average all ripples.
+mdam=mean(dam(:)); %Mean value 
+sdam=std(dam(:));
 
-    [ zmin100, zmax100] = ft_getminmax(cfg, FG3);
+%     FG3=freq3;
+%     FG3.time=[-.05:.001:.05];
+%     FG3.powspctrm=freq3.powspctrm(:,:,:,1+50:end-50);
+% 
+%     [ zmin100, zmax100] = ft_getminmax(cfg, FG3);
 end
 
