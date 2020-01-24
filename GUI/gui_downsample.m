525edcf7dbbc4b0d1fbee9df28ce0f8b7546f6d7
Aleman-Z <jraleman10@gmail.com>
2020-01-24 17:19:25 +0100

    Update gui_downsample.m

--- GUI/gui_downsample.m
+++ GUI/gui_downsample.m
@@ -39,11 +39,6 @@ if ~isempty(stage)
         stage=strsplit(stage,',');
     end
 end
-% if ~isempty(stage(contains(an,',')))
-%     stage=stage(contains(an,','));
-%     stage=stage{1};
-%     stage=strsplit(stage,',');
-% end
 
 
 %Adds trials containing an initial capital letter.
@@ -69,9 +64,6 @@ answer = inputdlg(prompt,'Input',[2 30],{''},opts);
 Rat=str2double(answer{1});
 % Rat=str2num(answer{1});
 
-%xo
-%for RAT=1:length(rats) %4
-% Rat=rats(RAT); 
 
 %GET EPHYS AND NEW FOLDER.
 dname=uigetdir([],strcat('Select folder with Ephys data for Rat',num2str(Rat)));
@@ -117,21 +109,9 @@ end
 iii=1;
 % for iii=4:length(labelconditions) 
  while iii<=length(labelconditions)  
-%     if Rat==9 && strcmp(labelconditions{iii},'OR_N')
-%         labelconditions{4}='OR+NOV';
-%     end
-%     if Rat==9 && strcmp(labelconditions{iii},'HC')
-%         labelconditions{iii}='homecage';
-%     end
 
 cd(dname)
-% aa=getfolder;
 
-% if contains(aa{1},'rat') || contains(aa{1},'Rat')
-%     
-% end
-
-%xo
 if size(label1,1)~=3
     [BB,labelconditions,labelconditions2]=select_folder(Rat,iii,labelconditions,labelconditions2);
     cd(BB)
@@ -141,12 +121,6 @@ else
 end
 
 %%
-%A=getfolder;
-%if isempty(A)
-%    cd ..
-%     xo
-%     A=getfolder;
-%end
 
 %Look for trial
 if ~isempty(stage)
@@ -179,19 +153,6 @@ if ~isempty(stage)
 end
 
 A=A.';
-%%
-% for n=1:size(A,1)
-%     ch=A{n};
-%     ch=ch(~isletter(ch));
-%     ch=strsplit(ch,'_')
-%     ch=ch(~cellfun('isempty',ch));
-%     ch=ch(~cellfun(@isnumeric,ch));
-%     ch2=cellfun('length',ch);
-%     ch=ch(ch2~=1);
-%     ch=strrep(ch,'-','+');
-%     ch=strrep(ch,'_','+');
-%     B(n,:)=cellfun(@str2num,ch);
-% end
 
 %% Label suggestion (Not used when All-trials option was selected)
 str2=cell(size(A,1),1);
@@ -233,9 +194,6 @@ c.String =sprintf('%s\n%s','Edit the Label column with the correct trial index a
 c.FontSize=10;
 c.FontAngle='italic';
 
-% label = uilabel(f,...
-%     'Position',[100 164 100 15],...
-%     'Text','Enter Comments:');
 uit = uitable(f);
 % d = {A,str2};
 uit.Data = [A str2];
@@ -244,8 +202,6 @@ uit.ColumnWidth= {200,50};;
 % uit.Position = [20 20 258 78];
 
 
-% t = uitable('Parent',f,'Data',dat,'ColumnName',cnames,... 
-%             'RowName',rnames,'Position',[20 280 450 100]);
         
 set(uit,'ColumnEditable',true(1,2))
 h = uicontrol('Position',[350 20 100 40],'String','Confirm',...
@@ -261,30 +217,10 @@ str1=str1(not(cellfun('isempty',str2)));
 A=A(not(cellfun('isempty',str2)));
 str2=str2(not(cellfun('isempty',str2)));
 %%
-% if strcmp(BB,'Study_day7_OR_N_1_2mar2018')
-% cd(BB)    
-% end
-% %xo
-% if Rat==6 || Rat==9 || Rat==11
-% [str1,str2]=select_trial('Pre',Rat,stage);    
-% else
-% [str1,str2]=select_trial('pre',Rat,stage);        
-% end
-% xo
-% %Reverse order of wrong strings. 
-% S=sum(cell2mat(cellfun(@(x) strfind(x,'pre-sleep'), str1,'UniformOutput',0)));
-% if S==1 && length(str1)>1
-% str1=sortrows(str1,[-1 1]);    
-% end
 
-% str2={'PT5'};
 F=waitbar(0,'Please wait...');
 for num=1:length(str1)
-    %length(str1)
        
-% if strcmp(BB,'Study_day7_OR_N_1_2mar2018') && num>1
-% cd(BB)    
-% end
  chfol=getfolder;
  if length(chfol)==1
      cd(chfol{1})
@@ -293,44 +229,10 @@ for num=1:length(str1)
     cd(str1{num,1});
 %     xo
  if iter_no_saving~=1   
-%         if Rat==11
-% 
-%             if Rat==11 && num==5 && iii==3
-%             [ax1, ~, ~] = load_open_ephys_data_faster('100_AUX1_2_0.continuous');
-%             [ax2, ~, ~] = load_open_ephys_data_faster('100_AUX2_2_0.continuous');
-%             [ax3, ~, ~] = load_open_ephys_data_faster('100_AUX3_2_0.continuous'); 
-%             else
-% 
-%             [ax1, ~, ~] = load_open_ephys_data_faster('100_AUX1_0.continuous');
-%             [ax2, ~, ~] = load_open_ephys_data_faster('100_AUX2_0.continuous');
-%             [ax3, ~, ~] = load_open_ephys_data_faster('100_AUX3_0.continuous');   
-% 
-%             end
-% 
-% 
-%         else
-%             [ax1, ~, ~] = load_open_ephys_data_faster('100_AUX1.continuous');
-%             [ax2, ~, ~] = load_open_ephys_data_faster('100_AUX2.continuous');
-%             [ax3, ~, ~] = load_open_ephys_data_faster('100_AUX3.continuous');    
-%         end
 
-
-
-        % Verifying time
-%          l=length(ax1); %samples
-%         % t=l*(1/fs); %  2.7276e+03  seconds
-%         % Equivalent to 45.4596 minutes
-%         t=1:l;
-%         t=t*(1/fs);
-
-%         sos=ax1.^2+ax2.^2+ax3.^2;    
-%         clear ax1 ax2 ax3 
  
         Wn=[fs_new/fs ]; % Cutoff=fs_new/2 Hz. 
         [b,a] = butter(3,Wn); %Filter coefficients for LPF.
-% 
-%         sos=filtfilt(b,a,sos);
-%         sos=decimator(sos,20);
 
     vr=getfield(channels,strcat('Rat',num2str(Rat)));%Electrode channels. 
 
@@ -339,9 +241,6 @@ for num=1:length(str1)
 %     cfold=cfold(cellfun(@(x) contains(x,'CH'),cfold));    
     cfold=cfold(cellfun(@(x) ~isempty(strfind(x,'CH')),cfold));
     
-%     cf1=cfold(cellfun(@(x) contains(x,num2str(vr(1))),cfold));
-%     cf2=cfold(cellfun(@(x) contains(x,num2str(vr(2))),cfold));
-%     cf1=cfold(cellfun(@(x) ~isempty(strfind(x,['CH' num2str(vr(1))])),cfold));
     cf1=[cfold(cellfun(@(x) ~isempty(strfind(x,['CH' num2str(vr(1)) '.'])),cfold)) cfold(cellfun(@(x) ~isempty(strfind(x,['CH' num2str(vr(1)) '_'])),cfold))];
 
 %     cf2=cfold(cellfun(@(x) ~isempty(strfind(x,['CH' num2str(vr(2))])),cfold));
@@ -478,15 +377,7 @@ end
     HPC=filtfilt(b,a,HPC);
     HPC=downsample(HPC,fs/fs_new);
 
-
-%     [HPC, ~, ~] = load_open_ephys_data_faster(cf1{1});    
-%     HPC=filtfilt(b,a,HPC);
-%     HPC=decimator(HPC,20);
-
     %PFC
-%     [PFC, ~, ~] = load_open_ephys_data_faster(cf2{1});
-%     PFC=filtfilt(b,a,PFC);
-%     PFC=decimator(PFC,20);
     [PFC, ~, ~] = load_open_ephys_data_faster(cf2{1});
     PFC=filtfilt(b,a,PFC);
     PFC=downsample(PFC,fs/fs_new);
@@ -518,16 +409,6 @@ if size(label1,1)~=3 %Instead of Plusmaze
 end
 %xo
 
-% cd(stage(1))
-% mkdir(stage{1})
-% mkdir(stage{2})
-
-% G=getfolder;
-% if sum(cellfun(@(x) strcmp(x,'Pre_trial'), G))>=1
-% rmdir Pre_trial 
-% rmdir Post_trial
-% end
-
 if ~exist(str2{num}, 'dir')
    mkdir(str2{num})
 end
@@ -540,8 +421,6 @@ if iter_no_saving~=1
 if size(label1,1)==3
  save(['PAR_' cf3{1} '.mat'],'PAR')
 end 
-%  save('HPC.mat','HPC')
-%  save('sos.mat','sos')
  ftext = fopen( str1{num}, 'w' );  
  fclose(ftext);
 end
@@ -557,42 +436,6 @@ else
 cd(strcat(dname,'/',BB))    
 end
 
-
-%  if Rat<9
-%      cd(strcat('F:\ephys\rat',num2str(Rat),'\',BB))
-%  else
-%      cd(strcat('G:\ephys'))
-%      aa=getfolder;
-%      if Rat==9
-%          cd(strcat(aa{2},'/',BB))
-%      else
-%          cd(strcat(aa{1},'/',BB))
-%      end
-%  end
- 
-%     %%
-%    [vtr]=findsleep(sos,median(sos)/100,t); %Threshold. 0.006
-%
-%     plot(sos(100000/2:400000))
-%     hold on
-%     stripes((vtr(100000/2:400000)),0.5)
-    %%
-%     vin=find(vtr~=1);
-
-% xo
-% [a1,a2] = unique(vtr);
-% 
-% [G,ID] = findgroups(vtr);
-% out = double(diff([~vtr(1);vtr(:)]) == 1);
-% v = accumarray(cumsum(out).*vtr(:)+1,1);
-% out(out == 1) = v(2:end);
-    
-    %Create transition matrix
-%    xo
-% if size(label1,1)==3        
-%     cd ..  % Only for Plusmaze data
-% end
-
 progress_bar(num,length(str1),F)
 end
 
@@ -606,9 +449,3 @@ end
 end
 
 end
-
-
-
-
-
-
