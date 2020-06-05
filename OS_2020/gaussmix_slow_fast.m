351ad0d67a729fe6a74aeeb16bcccea08117a360
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2020-06-05 17:26:19 +0200

    gaussmix_slow_fast

--- OS_2020/gaussmix_slow_fast.m
+++ OS_2020/gaussmix_slow_fast.m
@@ -1,9 +1,11 @@
-function[th]=gaussmix_slow_fast(data)
+function[cutoff,std_TH]=gaussmix_slow_fast(data)
 
 if size(data,1)< size(data,2)
  data=data.';   
 end
 
+%Run 100 times.
+for i=1:100
 % Fit a gaussian mixture model
 % obj = fitgmdist(data,2);
 
@@ -20,8 +22,10 @@ idx = cluster(obj,data);
 cluster1 = data(idx == 1,:);
 cluster2 = data(idx == 2,:);
 
-%Find threshold value
+%Find threshold value and standard deviation of the calculation.
 th=min([max(cluster1) max(cluster2)]);
-
-
+TH(i)=th;
+end
+cutoff=median(TH);
+std_TH=std(TH);
 end
