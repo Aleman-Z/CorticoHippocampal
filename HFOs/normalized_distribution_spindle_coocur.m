3c388e89569d7e6c77d1f8e705b82a21b7c30c6d
Aleman-Z <r.a.aleman.zapata@student.tue.nl>
2020-08-03 17:45:37 +0200

    Update normalized_distribution_spindle_coocur.m

--- HFOs/normalized_distribution_spindle_coocur.m
+++ HFOs/normalized_distribution_spindle_coocur.m
@@ -99,3 +99,113 @@ xlim([-6 6])
 printing(['Anew_PFC_all' '_' labelconditions{n}])
 %printing(['new_PFC_g1' '_' 'across_all'])
 close all
+%% NEWEST
+Norm_Cohfos2_PAR_g1_24=Cohfos1_PAR_all_g1-cellfun('length',Cohfos1_PAR_g1_post).';  %1x4
+Norm_Cohfos2_PAR_g1_24=Norm_Cohfos2_PAR_g1_24./std(Cohfos1_PAR_all_g1.').';
+
+Norm_Cohfos2_PAR_g2_24=Cohfos1_PAR_all_g2-cellfun('length',Cohfos1_PAR_g2_post).';  %1x4
+Norm_Cohfos2_PAR_g2_24=Norm_Cohfos2_PAR_g2_24./std(Cohfos1_PAR_all_g2.').';
+
+Norm_Cohfos2_PFC_g1_24=Cohfos1_PFC_all_g1-cellfun('length',Cohfos1_PFC_g1_post).';  %1x4
+Norm_Cohfos2_PFC_g1_24=Norm_Cohfos2_PFC_g1_24./std(Cohfos1_PFC_all_g1.').';
+
+Norm_Cohfos2_PFC_g2_24=Cohfos1_PFC_all_g2-cellfun('length',Cohfos1_PFC_g2_post).';  %1x4
+Norm_Cohfos2_PFC_g2_24=Norm_Cohfos2_PFC_g2_24./std(Cohfos1_PFC_all_g2.').';
+%%
+save('post_Norm_spindles_hfos_24','Norm_Cohfos2_PAR_g1_24','Norm_Cohfos2_PAR_g2_24',...
+    'Norm_Cohfos2_PFC_g1_24','Norm_Cohfos2_PFC_g2_24');
+%%
+%% NORM for hpc ripples
+Norm_Cohfos2_PAR_24_all=Cohfos2_PAR_hpc_all-cellfun('length',Cohfos2_PAR_hpc_post).';  %1x4
+Norm_Cohfos2_PAR_24_all=Norm_Cohfos2_PAR_24_all./std(Cohfos2_PAR_hpc_all.').';
+
+Norm_Cohfos2_PAR_24_unique=Cohfos2_PAR_hpc_unique-(cell2mat(cellfun(@(equis) length(unique(equis)),Cohfos2_PAR_hpc_post,'UniformOutput',false))).';  %1x4
+Norm_Cohfos2_PAR_24_unique=Norm_Cohfos2_PAR_24_unique./std(Cohfos2_PAR_hpc_unique.').';
+
+Norm_Cohfos2_PFC_24_all=Cohfos2_PFC_hpc_all-cellfun('length',Cohfos2_PFC_hpc_post).';  %1x4
+Norm_Cohfos2_PFC_24_all=Norm_Cohfos2_PFC_24_all./std(Cohfos2_PFC_hpc_all.').';
+
+Norm_Cohfos2_PFC_24_unique=Cohfos2_PFC_hpc_unique-(cell2mat(cellfun(@(equis) length(unique(equis)),Cohfos2_PFC_hpc_post,'UniformOutput',false))).';  %1x4
+Norm_Cohfos2_PFC_24_unique=Norm_Cohfos2_PFC_24_unique./std(Cohfos2_PFC_hpc_unique.').';
+%%
+save('post_Norm_spindles_SWR_24','Norm_Cohfos2_PAR_24_all','Norm_Cohfos2_PAR_24_unique',...
+    'Norm_Cohfos2_PFC_24_all','Norm_Cohfos2_PFC_24_unique');
+%%
+%%
+set(0,'defaultAxesFontName', 'Arial')
+set(0,'defaultTextFontName', 'Arial')
+
+n=4;
+close all
+vec=[Norm_Cohfos2_PFC_g2_26([n],:);Norm_Cohfos2_PFC_g2_27([n],:);Norm_Cohfos2_PFC_g2_24([n],:)];
+%vec=[Norm_Cohfos2_PFC_g2_26([1:4],:);Norm_Cohfos2_PFC_g2_27([1:4],:);Norm_Cohfos2_PFC_g2_24([1:4],:)];
+vec=vec(:);
+% vec=vec(~isnan(vec));
+% vec=vec(~isinf(vec));
+
+[k1]=histogram(vec,[-5.5:5.5],'FaceColor',[0 0 0]);
+set(gca,'FontName','Arial')
+set(gca,'FontSize',10)
+ hold on
+    ylabel('Frequency','FontSize',10,'FontName','Arial')
+    xlabel('Count','FontSize',10,'FontName','Arial')
+set(gca,'FontName','Arial')
+set(gca,'FontSize',10)
+ax = gca;
+ax.YAxis.FontSize = 18 %for y-axis 
+ax.XAxis.FontSize = 18 %for y-axis 
+ax.XAxis.FontName='Arial';
+ax.XAxis.FontName='Arial';
+
+    Y1 = prctile(vec,5)
+    Y2 = prctile(vec,95)
+ylim([0 max(k1.Values)]);
+plot([0 0],[0 max(k1.Values)],'-r','LineWidth',2);
+plot([Y1 Y1],[0 max(k1.Values)],'-.k','LineWidth',2);
+plot([Y2 Y2],[0 max(k1.Values)],'-.k','LineWidth',2);
+
+xlim([-6 6])
+
+cd('post')
+printing(['post_new_PFC_g2' '_' labelconditions{n}])
+cd ..
+close all
+%%
+set(0,'defaultAxesFontName', 'Arial')
+set(0,'defaultTextFontName', 'Arial')
+
+n=4;
+close all
+vec=[Norm_Cohfos2_PFC_26_unique([n],:);Norm_Cohfos2_PFC_27_unique([n],:);Norm_Cohfos2_PFC_24_unique([n],:)];
+%vec=[Norm_Cohfos2_PFC_g1_26([1:4],:);Norm_Cohfos2_PFC_g1_27([1:4],:);Norm_Cohfos2_PFC_g1_24([1:4],:)];
+vec=vec(:);
+% vec=vec(~isnan(vec));
+% vec=vec(~isinf(vec));
+
+[k1]=histogram(vec,[-5.5:5.5],'FaceColor',[0 0 0]);
+set(gca,'FontName','Arial')
+set(gca,'FontSize',10)
+ hold on
+    ylabel('Frequency','FontSize',10,'FontName','Arial')
+    xlabel('Count','FontSize',10,'FontName','Arial')
+set(gca,'FontName','Arial')
+set(gca,'FontSize',10)
+ax = gca;
+ax.YAxis.FontSize = 18 %for y-axis 
+ax.XAxis.FontSize = 18 %for y-axis 
+ax.XAxis.FontName='Arial';
+ax.XAxis.FontName='Arial';
+
+    Y1 = prctile(vec,5)
+    Y2 = prctile(vec,95)
+ylim([0 max(k1.Values)]);
+plot([0 0],[0 max(k1.Values)],'-r','LineWidth',2);
+plot([Y1 Y1],[0 max(k1.Values)],'-.k','LineWidth',2);
+plot([Y2 Y2],[0 max(k1.Values)],'-.k','LineWidth',2);
+xlim([-6 6])
+%%
+cd('post')
+printing(['post_Anew_PFC_unique' '_' labelconditions{n}])
+cd ..
+%printing(['new_PFC_g1' '_' 'across_unique'])
+close all
