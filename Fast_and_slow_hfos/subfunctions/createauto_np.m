6b37db11be911a5b1e5de4edf1defe079da7e11e
Aleman-Z <jraleman10@gmail.com>
2020-11-04 00:54:42 +0100

    GL_granger

--- Fast_and_slow_hfos/subfunctions/createauto_np.m
+++ Fast_and_slow_hfos/subfunctions/createauto_np.m
@@ -1,20 +1,20 @@
 function [granger]=createauto_np(data1,freqrange,condition)
+%Non-parametric granger
     cfg           = [];
     cfg.method    = 'mtmfft';
     cfg.taper     = 'dpss'; 
     cfg.output    = 'fourier'; 
-    cfg.tapsmofrq = 2; %1/1.2
-%     cfg.tapsmofrq = 4; %1/1.2
+    cfg.tapsmofrq = 2; 
     
     cfg.pad = 10;
     cfg.foi=freqrange;
-    %[0:1:500]
+
     freq          = ft_freqanalysis(cfg, data1);
 
     cfg           = [];
     cfg.method    = 'granger';
     
-    if strcmp('yes',condition)
+    if strcmp('yes',condition) %Conditional?
         cfg.granger.feedback    = 'yes';
         cfg.granger    = [];
         cfg.granger.conditional    = 'yes'; %Switch to no for pairwise. 
--- Fast_and_slow_hfos/subfunctions/getval_granger.m
+++ Fast_and_slow_hfos/subfunctions/getval_granger.m
@@ -1,6 +1,6 @@
-function [g1,g1_f,G,g_f,FB,FB1,n]=getval_granger(P,Q,labelconditions3,label1,s,w,fn)
+function [g1,g1_f,G,G_f,FB,FB1,n]=getval_granger(P,Q,labelconditions3,label1,s,w,fn)
 
-
+%Find minimum number of events
     n=min([length(P.(labelconditions3{1}).(label1{w}){s}) length(P.(labelconditions3{2}).(label1{w}){s})...
         length(P.(labelconditions3{3}).(label1{w}){s}) length(P.(labelconditions3{4}).(label1{w}){s})]);
     
@@ -29,38 +29,30 @@ for condition=1:length(labelconditions3)
     if length(q)>1000
         q=q(1:1000);
         p=p(1:1000);
-%         q_nl=q_nl(1:1000);
-%         p_nl=p_nl(1:1000);
     end
 
     if w==1 %HPC-centered ripples.
          p=cellfun(@(equis1) flip(equis1),p,'UniformOutput',false);
          q=cellfun(@(equis1) flip(equis1),q,'UniformOutput',false);
-%          p_nl=cellfun(@(equis1) flip(equis1),p_nl,'UniformOutput',false);
-%          q_nl=cellfun(@(equis1) flip(equis1),q_nl,'UniformOutput',false);
     end
     
     ro=1200;
-    [gran,gran1,grangercon]=gc_paper(p,create_timecell(ro,length(p)),'Wideband',ro,10,[0:1:300],fn);
+    %Compute non-parametric, parametric and conditional spectral GC.
+    [gran,gran1,grangercon]=getgranger(p,create_timecell(ro,length(p)),'Wideband',ro,10,[0:1:300],fn);
     
+    %Extract granger spectrums
     G{condition}=gran.grangerspctrm;%Non-parametric (Pairwise)
     g1{condition}=gran1.grangerspctrm;%Parametric
     g2{condition}=grangercon.grangerspctrm;%Non-parametric (Conditional)
     
+ %Compute mean granger for different frequency bands.   
 [FB{condition}]=gc_freqbands(gran,0,'granger');%Non-parametric (Pairwise)
 [FB1{condition}]=gc_freqbands(gran1,0,'granger');%Parametric
 
     
 end
-g_f=gran.freq;
+%Frequencies _f
+G_f=gran.freq;
 g1_f=gran1.freq;
- 
-% granger_paper4(g1,g1_f,labelconditions3,[0 300]) %All
-% printing(['Parametric'])
-% close all
-% 
-% granger_paper4(G,g_f,labelconditions3,[0 300]) %All
-% printing(['Non_parametric'])
-% close all
-                 
+                  
 end
