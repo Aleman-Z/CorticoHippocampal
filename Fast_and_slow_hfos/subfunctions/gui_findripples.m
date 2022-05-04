b80ab95584f5a7b55b51bb8b2eb39425b5c37021
Aleman-Z <jraleman10@gmail.com>
2022-05-04 13:18:20 +0200

    add ripple timestamps wrt recording

--- Fast_and_slow_hfos/subfunctions/gui_findripples.m
+++ Fast_and_slow_hfos/subfunctions/gui_findripples.m
@@ -1,17 +1,20 @@
-function [ripple,RipFreq,rip_duration,Mx,timeasleep,sig,Ex,Sx,ripple_multiplets,RipFreq_multiplets,rip_duration_multiplets,sig_multiplets,M_multiplets,V,Mono]=gui_findripples(CORTEX,states,xx,tr,multiplets,fn)
+function [ripple,RipFreq,rip_duration,Mx,timeasleep,sig,Ex,Sx,ripple_multiplets,RipFreq_multiplets,rip_duration_multiplets,sig_multiplets,M_multiplets,V,Mono,Sx_ti,Ex_ti,Mx_ti]=gui_findripples(CORTEX,states,xx,tr,multiplets,fn)
     %Band pass filter design:
     Wn1=[100/(fn/2) 300/(fn/2)]; % Cutoff=100-300 Hz
     [b1,a1] = butter(3,Wn1,'bandpass'); %Filter coefficients
     Wn1=[320/(fn/2)]; % Cutoff=320 Hz
     [b2,a2] = butter(3,Wn1); %Filter coefficients
+    ti_main=[0:length(CORTEX)-1]*(1/fn);
 %Rearrange signal in 1 sec epochs.
         e_t=1;
         e_samples=e_t*(fn); %fs=1kHz
         ch=length(CORTEX);
         nc=floor(ch/e_samples); %Number of epochsw
         NC=[];
+        NC_ti_main=[];
         for kk=1:nc
           NC(:,kk)= CORTEX(1+e_samples*(kk-1):e_samples*kk);
+          NC_ti_main(:,kk)= ti_main(1+e_samples*(kk-1):e_samples*kk);
         end
         %Find if epoch is NREM (state=3)
         vec_bin=states;
@@ -24,6 +27,7 @@ function [ripple,RipFreq,rip_duration,Mx,timeasleep,sig,Ex,Sx,ripple_multiplets,
     %Extract NREM epochs    
     for epoch_count=1:length(v_index)
     v{epoch_count,1}=reshape(NC(:, v_index(epoch_count):v_index(epoch_count)+(v_values(1,epoch_count)-1)), [], 1);
+    v_ti{epoch_count,1}=reshape(NC_ti_main(:, v_index(epoch_count):v_index(epoch_count)+(v_values(1,epoch_count)-1)), [], 1);
     end
     %Filter
     V=cellfun(@(equis) filtfilt(b2,a2,equis), v ,'UniformOutput',false);
@@ -32,17 +36,28 @@ function [ripple,RipFreq,rip_duration,Mx,timeasleep,sig,Ex,Sx,ripple_multiplets,
     timeasleep=sum(cellfun('length',V))*(1/fn)/60; % In minutes
     %NREM epochs without OpenEphys BitVolt factor
     signal2=cellfun(@(equis) times((1/0.195), equis)  ,Mono,'UniformOutput',false);
+
     %Timestamps for ripple/hfo detector
     ti=cellfun(@(equis) reshape(linspace(0, length(equis)-1,length(equis))*(1/fn),[],1) ,signal2,'UniformOutput',false);
     
     %If HPC find ripples, else find hfos.
     %Finds start, end and peak timestamp.
     if strcmp(xx{1},'HPC')
-    [Sx,Ex,Mx] =cellfun(@(equis1,equis2) findRipples(equis1, equis2, tr(1), (tr(1))*(1/2), [] ), signal2,ti,'UniformOutput',false);
+    [Sx,Ex,Mx] =cellfun(@(equis1,equis2) findRipplesLisa(equis1, equis2, tr(1), (tr(1))*(1/2), fn ), signal2,ti,'UniformOutput',false);
     else
-    [Sx,Ex,Mx] =cellfun(@(equis1,equis2) findHFOs(equis1, equis2, tr(2), (tr(2))*(1/2), [] ), signal2,ti,'UniformOutput',false);
+    [Sx,Ex,Mx] =cellfun(@(equis1,equis2) findRipplesLisa2020(equis1, equis2, tr(2), (tr(2))*(1/2), fn ), signal2,ti,'UniformOutput',false);
     end
-        
+    
+    % TIMESTAMPS WITH RESPECT TO RECORDING TIME.
+    %If HPC find ripples, else find hfos.
+    %Finds start, end and peak timestamp.
+    if strcmp(xx{1},'HPC')
+    [Sx_ti,Ex_ti,Mx_ti] =cellfun(@(equis1,equis2) findRipplesLisa(equis1, equis2, tr(1), (tr(1))*(1/2), fn ), signal2,v_ti,'UniformOutput',false);
+    else
+    [Sx_ti,Ex_ti,Mx_ti] =cellfun(@(equis1,equis2) findRipplesLisa2020(equis1, equis2, tr(2), (tr(2))*(1/2), fn ), signal2,v_ti,'UniformOutput',false);
+    end
+
+    
     %Multiplets detection
     for l=1:length(Mx)
          hfo_sequence=ConsecutiveOnes(diff(Mx{l})<=0.300);
@@ -77,7 +92,7 @@ function [ripple,RipFreq,rip_duration,Mx,timeasleep,sig,Ex,Sx,ripple_multiplets,
 %Get traces of events detected    
     for l=1:length(Sx)
          sig{l}=getsignal(Sx,Ex,ti,Mono,l);
-%        sig{l}=getsignal(Sx,Ex,ti,V,l);
+%         sig{l}=getsignal(Sx,Ex,ti,V,l);
         for ll=1:length(multiplets)
             eval(['sig_' multiplets{ll} '_1{l}=getsignal(Sx_' multiplets{ll} '_1,Ex_' multiplets{ll} '_1,ti,Mono,l);'])
 %             eval(['sig_multiplets.(' 'multiplets{ll}' ')=getsignal(Sx_' multiplets{ll} '_1,Ex_' multiplets{ll} '_1,ti,Mono,l);'])
