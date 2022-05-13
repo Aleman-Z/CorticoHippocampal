99f4121d5ebb5809c3ad7d7a713c1f043f958231
Aleman-Z <jraleman10@gmail.com>
2022-05-13 16:12:23 +0200

    Update create_timecell.m

--- Fast_and_slow_hfos/subfunctions/create_timecell.m
+++ Fast_and_slow_hfos/subfunctions/create_timecell.m
@@ -1,10 +1,10 @@
 
-function [C]=create_timecell(ro,leng)
+function [C]=create_timecell(ro,leng,fn)
 %create_timecell(ro,leng)
 %iNPUTS:
 %ro:1200
 %leng:length(p)
-    fn=1000;
+    %fn=1000;
     vec=-ro/fn:1/fn:ro/fn;
     C    = cell(1, leng);
     C(:) = {vec};
