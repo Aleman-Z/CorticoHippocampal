b20eecf25b50aa9923628985f8b0ad52f52d38e0
Aleman-Z <jraleman10@gmail.com>
2022-05-13 12:49:41 +0200

    granger_Nayanika

--- Fast_and_slow_hfos/GL_granger_Nayanika.m
+++ Fast_and_slow_hfos/GL_granger_Nayanika.m
@@ -437,9 +437,9 @@ labelconditions3=labelconditions3.';
 s=1; %Slow Cohfos. s=1 cooccurrent. s=2 singles.
 w=3; %w=3 PAR. 
 tf=1; % tf=1 to compute time frequency granger
-%[g1,g1_f,G,G_f,FB,FB1]=getval_granger(SP,SQ,labelconditions3,label1,s,w,fn);
-%[n, g1,g1_f,G,G_f,FB,FB1]=getval_granger(SP,SQ,labelconditions2,label1,s,w,fn, tf);
-[n, g_tf1]=getval_granger(SP,SQ,labelconditions2,label1,s,w,fn, tf);
+%[g1,g1_f,G,G_f,FB,FB1]=getval_granger_Nayanika(SP,SQ,labelconditions3,label1,s,w,fn);
+%[n, g1,g1_f,G,G_f,FB,FB1]=getval_granger_Nayanika(SP,SQ,labelconditions2,label1,s,w,fn, tf);
+[n, g_tf1]=getval_granger_Nayanika(SP,SQ,labelconditions2,label1,s,w,fn, tf);
 plot_granger_tf(g_tf1, 'Slow HFO');
 %g1 :Parametric Granger spectrum
 %g1_f :Parametric frequencies
@@ -490,8 +490,8 @@ writematrix('Slow HFO: Parametric 20-300 Hz','granger_causality.xls', 'sheet',4,
 
 %-------------
 w=3; %PAR
-%[n2, g1,g1_f,G,G_f,FB,FB1]=getval_granger(FP,FQ,labelconditions2,label1,s,w,fn, tf);
-[n2, g_tf2]=getval_granger(FP,FQ,labelconditions2,label1,s,w,fn, tf);
+%[n2, g1,g1_f,G,G_f,FB,FB1]=getval_granger_Nayanika(FP,FQ,labelconditions2,label1,s,w,fn, tf);
+[n2, g_tf2]=getval_granger_Nayanika(FP,FQ,labelconditions2,label1,s,w,fn, tf);
 plot_granger_tf(g_tf2, 'Fast HFO');
 
 %labelconditions3{1}='baseline';
@@ -534,8 +534,8 @@ writematrix('Fast HFO: Parametric 20-300 Hz','granger_causality.xls', 'sheet',4,
 %labelconditions3{1}='nl';
 %s=2; %Slow singles
 w=1;%HPC
-%[n3,g1,g1_f,G,G_f,FB,FB1]=getval_granger(P,Q,labelconditions2,label1,s,w,fn, tf);
-[n3, g_tf3]=getval_granger(P,Q,labelconditions2,label1,s,w,fn, tf);
+%[n3,g1,g1_f,G,G_f,FB,FB1]=getval_granger_Nayanika(P,Q,labelconditions2,label1,s,w,fn, tf);
+[n3, g_tf3]=getval_granger_Nayanika(P,Q,labelconditions2,label1,s,w,fn, tf);
 %plot_granger_tf(g_tf3, 'HPC');
 plot_granger_tf(g_tf1, g_tf2, g_tf3,'Slow HFO', 'Fast HFO', 'ripples');
 
