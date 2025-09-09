3f92bf69239e33c56837c416286ebf68c98343f0
Adrián Alemán-Zapata <jraleman10@gmail.com>
2025-09-08 18:43:41 -0600

    Update barplot_COH.m

--- barplot_COH.m
+++ barplot_COH.m
@@ -1,118 +1,53 @@
-function [coh]=barplot_COH(q,timecell,freqrange)
-%[100:1:300]
-
-ft_data1 = [];
-ft_data1.fsample = 1000;
-%tt=linspace(-5,5,1000);
-%ft_data1.trial = q(1:1); % q should be larger than +/-500 ms. Better to use 1 sec. 
-%ft_data1.trial = q(1,end-199:end); % q should be larger than +/-500 ms. Better to use 1 sec. 
-ft_data1.trial = q(1,1:end); % q should be larger than +/-500 ms. Better to use 1 sec. 
-
-
-%ft_data1.time = (timecell(1,end-199:end));
-ft_data1.time = (timecell(1,1:end));
-
-%ft_data1.time = {[tt;tt;tt;tt]};
-
-%ft_data1
-
-%ft_data1.label = {'Hippo'; 'Parietal'; 'PFC';'REF'};
-ft_data1.label = {'Hippo'; 'Parietal'; 'PFC'};
-
-
-% Compute Multitaper
-
-cfg = [];
-cfg.method = 'mtmconvol';
-%cfg.taper = 'dpss';
-cfg.taper = 'hanning';
-cfg.pad='nextpow2';
-%cfg.foi = [100:50:300];
-cfg.foi = freqrange;
-
-
-cfg.t_ftimwin = 0.2*ones(size(cfg.foi));
-%  da=freqrange(end)-freqrange(end-1);
-%  da=1/da;
-%  
-% cfg.t_ftimwin  = da./cfg.foi;
-% 
-cfg.tapsmofrq = 10;
-cfg.toi = [-1:.025:1];
-%cfg.toi = [-1:.01:1];
-
-
-%cfg.toi = [-0.8:.01:0.8];
-
-
-%cfg.toi = [-.4:.001:.4];
-cfg.keeptrials = 'yes';
-cfg.output         = 'powandcsd';
-%cfg.output         = 'fourier';
-
-
-freq = ft_freqanalysis(cfg, ft_data1);
-
-
-% cfg = [];
-% cfg.method = 'wavelet';
-% cfg.pad='nextpow2';
-% 
-% % cfg.taper = 'dpss';
-% %cfg.foi = [100:50:300];
-% cfg.foi = freqrange;
-% 
-% cfg.width=20;
-% cfg.gwidth=3;
-% 
-% 
-% %cfg.t_ftimwin = .2 * ones(size(cfg.foi));
-% cfg.tapsmofrq = 10;
-% %cfg.toi = [-.4:.1:.4];
-% %cfg.toi = [-.4:.001:.4];
-% cfg.toi = [-.5:.001:.5];
-% 
-% cfg.keeptrials = 'yes';
-% cfg.output         = 'powandcsd';
-% freq = ft_freqanalysis(cfg, ft_data1);
-
-
+function [coh] = barplot_COH(q, timecell, freqrange)
+% barplot_COH: Compute time-frequency coherence using FieldTrip
 %
-% Plot 
-%allscreen()
-% 
-% chanindx = find(strcmp(freq.label, 'Hippo'));
-% % imagesc(squeeze(freq.powspctrm(chanindx,:,:)));
-% imagesc(freq.time, freq. freq, squeeze(freq.powspctrm(chanindx,:,:)));
-% axis xy % flip vertically
-% colorbar
+% INPUTS:
+%   q          - data matrix (channels x time)
+%   timecell   - time vector (same length as q)
+%   freqrange  - frequency range, e.g. 100:1:300
+%
+% OUTPUT:
+%   coh        - coherence structure from ft_connectivityanalysis
+%
+% Example:
+%   coh = barplot_COH(q, timecell, 100:1:300);
+%
+% Requires FieldTrip on path.
 
-% 
-% chanindx = find(strcmp(freq.label,ft_data1.label{label} ));
-% imagesc(freq.time, freq. freq, squeeze(freq.powspctrm(1,chanindx,:,:)));
-% axis xy % flip vertically
-% colormap(jet(256))
-%  narrow_colorbar()
-% cfg=[];
-% %cfg.channel = label; % top figure
-% cfg.channel =ft_data1.label{label};
-% ft_singleplotTFR(cfg,  freq);
-% %colorbar()
-% colormap(jet(256))
+%% ---------------- Prepare data ----------------
+ft_data           = [];
+ft_data.fsample   = 1000;                     % Sampling rate
+ft_data.trial     = {q};                      % Wrap in cell for FieldTrip
+ft_data.time      = {timecell};               % Time vector
+ft_data.label     = {'Hippo'; 'Parietal'; 'PFC'}; % Channel labels
 
-%COHERENCE
-cfg           = [];
-cfg.method    = 'coh';
-coh           = ft_connectivityanalysis(cfg, freq);
+%% ---------------- Frequency analysis ----------------
+cfg               = [];
+cfg.method        = 'mtmconvol';              % multitaper convolution
+cfg.taper         = 'hanning';
+cfg.pad           = 'nextpow2';
+cfg.foi           = freqrange;                % Frequencies of interest
+cfg.t_ftimwin     = 0.2 * ones(size(cfg.foi));% Fixed 200 ms window
+cfg.tapsmofrq     = 10;                       % Frequency smoothing
+cfg.toi           = -1:0.025:1;               % Time of interest
+cfg.keeptrials    = 'yes';
+cfg.output        = 'powandcsd';              % Needed for coherence
 
-cfg           = [];
-cfg.parameter = 'cohspctrm';
-cfg.channel= {'Hippo'; 'Parietal'; 'PFC'};
+freq              = ft_freqanalysis(cfg, ft_data);
 
-cfg.zlim      = [0 1];
+%% ---------------- Coherence analysis ----------------
+cfg               = [];
+cfg.method        = 'coh';
+coh               = ft_connectivityanalysis(cfg, freq);
+
+%% ---------------- Plot coherence ----------------
+cfg               = [];
+cfg.parameter     = 'cohspctrm';
+cfg.channel       = {'Hippo'; 'Parietal'; 'PFC'};
+cfg.zlim          = [0 1];
+
 ft_connectivityplot(cfg, coh);
 title('Time-Frequency Coherence')
-
 colormap(jet(256))
 narrow_colorbar()
 
