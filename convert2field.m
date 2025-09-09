a974d8ad8c8e921df9e75167671e926f14b9439e
Adrián Alemán-Zapata <jraleman10@gmail.com>
2025-09-08 18:50:33 -0600

    Update convert2field.m

--- convert2field.m
+++ convert2field.m
@@ -1,35 +1,45 @@
-function [newdata]=convert2field(dat)
+function [newdata] = convert2field(dat)
+% convert2field: Convert custom data into FieldTrip format
+%
+% INPUT:
+%   dat - cell array where each cell is [channels x time] or [time x channels]
+%
+% OUTPUT:
+%   newdata - FieldTrip-compatible structure with fields:
+%             .trial    - cell array of trials (channels x time)
+%             .time     - cell array of time vectors
+%             .label    - channel labels
+%             .fsample  - sampling frequency
+%
+% Example:
+%   newdata = convert2field(myData);
 
-cc=cell(1,length(dat));
+%% ---------------- Convert trials ----------------
+nTrials = length(dat);
+cc = cell(1, nTrials);
 
-for i=1:length(dat)
-%     cc{i}=dat(:,:,i);
-    cc{i}=dat{i}.';
-
-    cc{i}=cc{i}.';
+for i = 1:nTrials
+    trialData = dat{i};
+    % Ensure orientation is channels x time
+    if size(trialData,1) < size(trialData,2)
+        trialData = trialData.'; 
+    end
+    cc{i} = trialData;
 end
 
-chc=size(dat);
+%% ---------------- Build output struct ----------------
+newdata.trial    = cc;
+newdata.fsample  = 1000;   % Hz (adjust if needed)
 
-newdata.trial=cc;
-newdata.fsample=1000;
-tee=0:(chc(2))-1;
-tee=tee*(1/200);
-tee=tee-1.2;
-tee=linspace(-1.2,1.2,2401)
-%newdata.time=tee;
+% Time vector (assuming fixed length = 2401 samples, -1.2 to 1.2 s)
+nSamples = size(cc{1},2);
+tee = linspace(-1.2,1.2,nSamples);
 
-for i=1:length(dat)
-   tim{i}=tee;
-end
-newdata.time=tim;
+tim = repmat({tee}, 1, nTrials);
+newdata.time = tim;
 
-
-%for i=1:chc(2)
-for i=1:3
-    
-label{1,i}=num2str(i);
-end
-newdata.label=label;
+% Channel labels
+nChannels = size(cc{1},1);
+newdata.label = arrayfun(@num2str, 1:nChannels, 'UniformOutput', false);
 
 end
